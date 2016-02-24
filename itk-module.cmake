ea79387f4c9abf643b12f346d3f9e9033bcfd9f2
Matt McCormick <matt.mccormick@kitware.com>
2016-02-23 22:27:40 -0500

    ENH: Add initial CMakeLists.txt and itk-module.cmake.

---
+++ itk-module.cmake
@@ -0,0 +1,15 @@
+set(DOCUMENTATION "This module provides a wrapper to an itk::Image in
+JavaScript when working with ITK and Emscripten. It is also used to generate a
+library of Emscription modules that provides IO for different ITK-supported
+file formats.
+")
+
+itk_module(BridgeJavaScript
+  DEPENDS
+    ITKCommon
+  TEST_DEPENDS
+    ITKTestKernel
+  EXCLUDE_FROM_DEFAULT
+  DESCRIPTION
+    "${DOCUMENTATION}"
+)
