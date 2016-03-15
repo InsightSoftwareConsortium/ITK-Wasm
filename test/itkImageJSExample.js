256c5fd215ae989ad7bb631c3a34752730fa8c4b
Matt McCormick <matt.mccormick@kitware.com>
2016-03-15 11:20:53 -0400

    ENH: Add tests.

--- test/itkImageJSExample.js
+++ test/itkImageJSExample.js
@@ -1,21 +1,19 @@
 
-if(process.argv.length < 4){
-	console.error("itkImageJS example, set random pixels to the image.");
-	console.error("Use mode: .nrrd or .nii files");
-	console.error(process.argv[0], process.argv[1], " inputImage outputImage");
-	return;
+if(process.argv.length < 4) {
+  console.error("itkImageJS example, set random pixels to the image.");
+  console.error("Use mode: .nrrd or .nii files");
+  console.error(process.argv[0], process.argv[1], " moduleDir inputImage outputImage");
+  process.exit(1);
 }
-
-var Module = require("../examples/js/itkImageJS.js");
-
-var imagejs = new Module.itkImageJS();
-
-var inputImage = process.argv[2];
-var outputImage = process.argv[3];
-
+var moduleDir = process.argv[2]
+var inputImage = process.argv[3];
+var outputImage = process.argv[4];
 console.log("Input image: ", inputImage);
 console.log("Output image: ", outputImage);
 
+var path = require("path");
+var Module = require(path.join(moduleDir, "itkImageJS.js"));
+var imagejs = new Module.itkImageJS();
 
 console.log("Reading image...");
 imagejs.MountDirectory(inputImage);
@@ -25,12 +23,12 @@ var dim = imagejs.GetDimensions();
 var dimensions = Module.HEAP32.subarray(dim, dim + 3);
 
 console.log("Random pixels every 2 slices...");
-for(var i = 0; i < dimensions[0]; i+=2){
-	for(var j = 0; j < dimensions[1]; j++){
-		for(var k = 0; k < dimensions[2]; k++){
-			imagejs.SetPixel(i, j, k, Math.random()*255);
-		}
-	}
+for(var i = 0; i < dimensions[0]; i+=2) {
+  for(var j = 0; j < dimensions[1]; j++) {
+    for(var k = 0; k < dimensions[2]; k++) {
+      imagejs.SetPixel(i, j, k, Math.random()*255);
+    }
+  }
 }
 console.log("Writing image...");
 imagejs.MountDirectory(outputImage);
