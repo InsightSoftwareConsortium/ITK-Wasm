ea7a6621462c85b45689bc9feb636abfcac35245
Matt McCormick <matt.mccormick@kitware.com>
2016-08-22 11:21:01 -0400

    ENH: Webpack config

---
+++ src/itk.js
@@ -0,0 +1,12 @@
+//var NodeESModuleLoader = require('node-es-module-loader');
+var SystemRegisterLoader = require('system-register-loader');
+
+var loader = new SystemRegisterLoader();
+
+//require.ensure(["../build/itk-imageio/itkPNGImageIOJSBinding.js"], function(require) {
+      //var a = require("../build/itk-imageio/itkPNGImageIOJSBinding.js");
+      //imageio = new a.itkPNGImageIO();
+      //console.log(imageio);
+      //imageio.SetFileName('myfile.png');
+      //console.log(imageio.GetFileName());
+//});
