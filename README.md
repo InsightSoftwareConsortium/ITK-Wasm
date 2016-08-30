642760a9797ad6532ec4cbf4b821d85c6b5c106f
Matt McCormick <matt.mccormick@kitware.com>
2016-08-29 22:50:55 -0400

    ENH: Create the dockcross script in the build command

---
+++ README.md
@@ -0,0 +1,12 @@
+# ITK Bridge JavaScript
+
+[![CircleCI](https://circleci.com/gh/InsightSoftwareConsortium/ITKBridgeJavaScript.svg?style=svg)](https://circleci.com/gh/InsightSoftwareConsortium/ITKBridgeJavaScript)
+
+## Hacking ITKBridgeJavaScript
+
+To build ITKBridgeJavaScript itself:
+
+```bash
+npm install
+npm run build
+```
--- build.js
+++ build.js
@@ -1,11 +1,42 @@
-const spawn = require('child_process').spawn;
+const fs = require('fs');
+const path = require('path');
 
-webpack_call = spawn('webpack', [], {
+try {
+  fs.mkdirSync('build');
+} catch(err) {
+  if (err.code != 'EEXIST') throw err;
+}
+
+const spawn = require('child_process').spawn;
+const dockcross = path.join('build', 'dockcross');
+try {
+  fs.statSync(dockcross);
+} catch(err) {
+  if (err.code == 'ENOENT') {
+    const output = fs.openSync(dockcross, 'w');
+    const dockerCall = spawn('docker', ['run', '--rm', 'insighttoolkit/bridgejavascript-test'], {
+      env: process.env,
+      stdio: [ 'ignore', output, null ]
+    });
+    dockerCall.on('exit', function(code) {
+      if(code != 0) {
+        process.exit(code);
+      }
+    });
+    fs.chmodSync(dockcross, '755');
+  }
+  else {
+    throw err;
+  }
+}
+
+
+webpackCall = spawn('webpack', [], {
   env: process.env,
   stdio: 'inherit'
 });
 
-webpack_call.on('close', function(code) {
+webpackCall.on('close', function(code) {
   process.exit(code);
 });
 
