cc1c1f7c09cb34c57f9341c38172830ee841bae4
Matt McCormick <matt.mccormick@kitware.com>
2016-09-26 22:12:53 -0400

    ENH: Use the mocha test framework

--- test/itkImageTest.js
+++ test/itkImageTest.js
@@ -3,8 +3,14 @@ const assert = require('assert');
 
 const itk = require(path.resolve(__dirname, '..', 'dist', 'itk.js'));
 
-let image = new itk.Image(2);
-assert(image.dimension === 2);
+describe('Image', function() {
+  describe('#dimension', function() {
+    it('should have the same dimension passed to the constructor', function() {
+      let image = new itk.Image(2);
+      assert(image.dimension === 2);
 
-image = new itk.Image(3);
-assert(image.dimension === 3);
+      image = new itk.Image(3);
+      assert(image.dimension === 3);
+    });
+  });
+});
