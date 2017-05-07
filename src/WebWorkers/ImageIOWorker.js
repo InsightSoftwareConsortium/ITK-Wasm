a4d2a97736691b5a8be681b0c058d22344ff5786
Matt McCormick <matt.mccormick@kitware.com>
2017-05-06 21:41:48 -0400

    ENH: Call readImageFile in the browser test

---
+++ src/WebWorkers/ImageIOWorker.js
@@ -0,0 +1,1 @@
+console.log('in the web worker')
--- src/itkreadImageFile.js
+++ src/itkreadImageFile.js
@@ -3,6 +3,8 @@
 const config = require('./itkConfig.js')
 
 // const loadEmscriptenModule = require('./itkloadEmscriptenModule.js')
+const ImageType = require('./itkImageType.js')
+const Image = require('./itkImage.js')
 
 const readImageFile = (file) => {
   return new Promise(function (resolve, reject) {
@@ -15,7 +17,9 @@ const readImageFile = (file) => {
       // const Module = loadEmscriptenModule(modulePath)
       // const image = readImageEmscriptenFSFile(Module, filePath)
       // resolve(image)
-      resolve(null)
+      let imageType = new ImageType(3)
+      let image = new Image(imageType)
+      resolve(image)
     } catch (err) {
       reject(err)
     }
