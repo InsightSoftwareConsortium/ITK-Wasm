52ce0fe13f201a0d6316cb2bd4db5dda5517bb60
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2017-05-24 13:37:05 -0600

    doc(github.io): Create infrastructure to generate project doc

---
+++ doc/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  baseUrl: '/ITKBridgeJavaScript',
+  work: './build-tmp',
+  examples: [],
+  config: {
+    title: 'ITKBridgeJavaScript',
+    description: '"ITK Bridge for JavaScript"',
+    subtitle: '"Leverage ITK algorithms and IO in JavaScript."',
+    author: 'Kitware Inc.',
+    timezone: 'UTC',
+    url: 'https://InsightSoftwareConsortium.github.io/ITKBridgeJavaScript',
+    root: '/ITKBridgeJavaScript/',
+    github: 'InsightSoftwareConsortium/ITKBridgeJavaScript',
+    google_analytics: 'UA-90338862-8',
+  },
+  copy: [],
+};
