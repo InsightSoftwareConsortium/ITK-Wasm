c091baa10e4d8270d6c2089f0a5ac6386b657aa3
Matt McCormick <matt.mccormick@kitware.com>
2018-03-08 14:21:31 -0500

    feat(MimeToMeshIO): Initial addition

---
+++ test/MimeToMeshIOTest.js
@@ -0,0 +1,8 @@
+import test from 'ava'
+// import path from 'path'
+
+// const MimeToIO = require(path.resolve(__dirname, '..', 'dist', 'MimeToMeshIO.js'))
+
+test('none mime types to test yet', t => {
+  t.pass()
+})
