f648034a9e79c9ffb2a5702d813d861ec57fdf9a
Matt McCormick <matt.mccormick@kitware.com>
2018-03-21 11:00:06 -0400

    doc(readImageFile): Add API documentation

--- doc/content/docs/index.md
+++ doc/content/docs/index.md
@@ -3,4 +3,4 @@ title: Overview
 
 *itk.js* combines [Emscripten](http://emscripten.org/) and [ITK](https://www.itk.org/) to enable high-performance spatial analysis in a JavaScript runtime environment.
 
-The project provides tools to build C/C++ code to JavaScript ([asm.js](http://asmjs.org/)) and [WebAssembly](http://webassembly.org/), transfer data efficently in and out of the Emscripten runtime, and asynchronously execute processing pipelines in a background thread.
+The project provides tools to a) build C/C++ code to JavaScript ([asm.js](http://asmjs.org/)) and [WebAssembly](http://webassembly.org/), b) bridge local filesystems, native JavaScript data structures, and traditional file formats, c) transfer data efficently in and out of the Emscripten runtime, and d) asynchronously execute processing pipelines in a background thread. *itk.js* can be used to execute [ITK](https://www.itk.org/), [VTK](https://www.vtk.org/) or arbitrary C++ codes in the browser or on a workstation / server with Node.js.
--- src/readImageFile.js
+++ src/readImageFile.js
@@ -1,17 +1,26 @@
-import WebworkerPromise from 'webworker-promise'
-import PromiseFileReader from 'promise-file-reader'
+import WebworkerPromise from 'webworker-promise';
+import PromiseFileReader from 'promise-file-reader';
 
-import config from './itkConfig'
+import config from './itkConfig';
 
-const worker = new window.Worker(config.itkModulesPath + '/WebWorkers/ImageIO.worker.js')
-const promiseWorker = new WebworkerPromise(worker)
+const worker = new window.Worker(
+  config.itkModulesPath + '/WebWorkers/ImageIO.worker.js'
+);
+const promiseWorker = new WebworkerPromise(worker);
 
 const readImageFile = (file) => {
-  return PromiseFileReader.readAsArrayBuffer(file)
-    .then(arrayBuffer => {
-      return promiseWorker.postMessage({ operation: 'readImage', name: file.name, type: file.type, data: arrayBuffer, config: config },
-        [arrayBuffer])
-    })
-}
+  return PromiseFileReader.readAsArrayBuffer(file).then((arrayBuffer) => {
+    return promiseWorker.postMessage(
+      {
+        operation: 'readImage',
+        name: file.name,
+        type: file.type,
+        data: arrayBuffer,
+        config: config,
+      },
+      [arrayBuffer]
+    );
+  });
+};
 
-export default readImageFile
+export default readImageFile;
