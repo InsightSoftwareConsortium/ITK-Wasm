26d023dca59530223a934fd47ff123cd81f5e81b
Matt McCormick <matt.mccormick@kitware.com>
2018-10-10 22:53:43 -0400

    doc(UnpkgIOExample): Update babel documentation

--- examples/UnpkgIO/webpack.config.js
+++ examples/UnpkgIO/webpack.config.js
@@ -1,44 +1,43 @@
-const path = require('path')
+const path = require('path');
 
-const webpack = require('webpack')
+const webpack = require('webpack');
 
-const entry = path.join(__dirname, './src/index.js')
-const outputPath = path.join(__dirname, './dist')
+const entry = path.join(__dirname, './src/index.js');
+const outputPath = path.join(__dirname, './dist');
 
-const packageJSON = require('./package.json')
-const itkVersion = packageJSON.dependencies.itk.substring(1)
-const cdnPath = 'https://unpkg.com/itk@' + itkVersion
+const packageJSON = require('./package.json');
+const itkVersion = packageJSON.dependencies.itk.substring(1);
+const cdnPath = 'https://unpkg.com/itk@' + itkVersion;
 
 module.exports = {
   node: {
-    fs: 'empty'
+    fs: 'empty',
   },
   entry,
   output: {
     path: outputPath,
     filename: 'index.js',
-    publicPath: cdnPath
+    publicPath: cdnPath,
   },
   module: {
     rules: [
       { test: entry, loader: 'expose-loader?index' },
-      { test: /\.js$/,
+      {
+        test: /\.js$/,
         use: {
           loader: 'babel-loader',
-          options: { presets: ['@babel/preset-env'] }
-          }
-      }
-    ]
+          options: { presets: ['@babel/preset-env'] },
+        },
+      },
+    ],
   },
   resolve: {
-    modules: [
-      path.resolve(__dirname, 'node_modules'),
-    ],
+    modules: [path.resolve(__dirname, 'node_modules')],
     alias: {
       './itkConfig$': path.resolve(__dirname, 'src', 'itkConfigCDN.js'),
     },
   },
   performance: {
-    maxAssetSize: 10000000
-  }
-}
+    maxAssetSize: 10000000,
+  },
+};
