40d794f5c2ff5bcb5df9c6c46f351d53f6f5df1e
Matt McCormick <matt.mccormick@kitware.com>
2019-01-26 17:01:36 -0500

    build(CircleCI): Update tagged image push

--- src/Docker/itk-js-base/build.sh
+++ src/Docker/itk-js-base/build.sh
@@ -4,13 +4,13 @@ script_dir="`cd $(dirname $0); pwd`"
 
 TAG=$(date '+%Y%m%d')-$(git rev-parse --short HEAD)
 
-docker build -t insighttoolkit/itk-js-base:latest \
+docker build "$@" -t insighttoolkit/itk-js-base:latest \
         --build-arg IMAGE=insighttoolkit/itk-js-base \
         --build-arg VCS_REF=`git rev-parse --short HEAD` \
         --build-arg VCS_URL=`git config --get remote.origin.url` \
         --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
         $script_dir
-docker build -t insighttoolkit/itk-js-base:${TAG} \
+docker build "$@" -t insighttoolkit/itk-js-base:${TAG} \
         --build-arg IMAGE=insighttoolkit/itk-js-base \
         --build-arg VERSION=${TAG} \
         --build-arg VCS_REF=`git rev-parse --short HEAD` \
--- src/Docker/itk-js/build.sh
+++ src/Docker/itk-js/build.sh
@@ -9,13 +9,13 @@ rsync -a --exclude=../../../src/Docker ../../../{include,src,CMakeLists.txt,itk-
 
 TAG=$(date '+%Y%m%d')-$(git rev-parse --short HEAD)
 
-docker build -t insighttoolkit/itk-js:latest \
+docker build "$@" -t insighttoolkit/itk-js:latest \
         --build-arg IMAGE=insighttoolkit/itk-js \
         --build-arg VCS_REF=`git rev-parse --short HEAD` \
         --build-arg VCS_URL=`git config --get remote.origin.url` \
         --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
         $script_dir
-docker build -t insighttoolkit/itk-js:${TAG} \
+docker build "$@" -t insighttoolkit/itk-js:${TAG} \
         --build-arg IMAGE=insighttoolkit/itk-js \
         --build-arg VERSION=${TAG} \
         --build-arg VCS_REF=`git rev-parse --short HEAD` \
