69a0dda93bf76082dee790b00f0e1ae4d60741b4
Matt McCormick <matt.mccormick@kitware.com>
2019-04-02 13:59:01 -0400

    build(CLI): Bump itk-js image to 20190402-8ce76a2

--- src/itk-js-cli.js
+++ src/itk-js-cli.js
@@ -38,7 +38,7 @@ const build = (sourceDir) => {
     process.exit(dockerVersion.status)
   }
 
-  let dockerImage = 'insighttoolkit/itk-js:20190317-5d260e3'
+  let dockerImage = 'insighttoolkit/itk-js:20190402-8ce76a2'
   if (program.commands[0].image) {
     dockerImage = program.commands[0].image
   }
