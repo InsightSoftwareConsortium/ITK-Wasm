c6558d825dc9ba631edf2b94bc3f145fdcd9d016
Matt McCormick <matt.mccormick@kitware.com>
2020-04-02 11:56:30 -0400

    perf(readImageDICOMFileSeries): Read sub-series in parallel
    A web worker pool is used to read sub-series in parallel wwhen singleSortedSeries is true. The
    resulting images are stacked together.
    
    BREAKING CHANGE: readImageDICOMFileSeries no longer takes a web worker as its first argument nor
    does it return the used worker.

--- src/readImageDICOMFileSeries.js
+++ src/readImageDICOMFileSeries.js
@@ -1,11 +1,13 @@
 import createWebworkerPromise from './createWebworkerPromise'
 import PromiseFileReader from 'promise-file-reader'
+import WorkerPool from './WorkerPool'
+import stackImages from './stackImages'
 
 import config from './itkConfig'
 
-const readImageDICOMFileSeries = async (
+const workerFunction = async (
   webWorker,
-  fileList,
+  fileDescriptions,
   singleSortedSeries = false
 ) => {
   let worker = webWorker
@@ -14,6 +16,28 @@ const readImageDICOMFileSeries = async (
     worker
   )
   worker = usedWorker
+
+  const transferables = fileDescriptions.map(description => {
+    return description.data
+  })
+  const message = {
+    operation: 'readDICOMImageSeries',
+    fileDescriptions: fileDescriptions,
+    singleSortedSeries,
+    config
+  }
+  const image = await webworkerPromise.postMessage(message, transferables)
+  return { image, webWorker: worker }
+}
+const numberOfWorkers = navigator.hardwareConcurrency ? navigator.hardwareConcurrency : 4
+const workerPool = new WorkerPool(numberOfWorkers, workerFunction)
+
+const seriesBlockSize = 8
+
+const readImageDICOMFileSeries = async (
+  fileList,
+  singleSortedSeries = false
+) => {
   const fetchFileDescriptions = Array.from(fileList, function (file) {
     return PromiseFileReader.readAsArrayBuffer(file).then(function (
       arrayBuffer
@@ -28,17 +52,21 @@ const readImageDICOMFileSeries = async (
   })
 
   const fileDescriptions = await Promise.all(fetchFileDescriptions)
-  const transferables = fileDescriptions.map(description => {
-    return description.data
-  })
-  const message = {
-    operation: 'readDICOMImageSeries',
-    fileDescriptions: fileDescriptions,
-    singleSortedSeries,
-    config
+  if (singleSortedSeries) {
+    const taskArgsArray = []
+    for (let index = 0; index < fileDescriptions.length; index += seriesBlockSize) {
+      const block = fileDescriptions.slice(index, index + seriesBlockSize)
+      taskArgsArray.push([block, singleSortedSeries])
+    }
+    const results = await workerPool.runTasks(taskArgsArray)
+    const images = results.map((result) => result.image)
+    const stacked = stackImages(images)
+    return { image: stacked, webWorkerPool: workerPool }
+  } else {
+    const taskArgsArray = [[fileDescriptions, singleSortedSeries]]
+    const results = await workerPool.runTasks(taskArgsArray)
+    return { image: results[0].image, webWorkerPool: workerPool }
   }
-  const image = await webworkerPromise.postMessage(message, transferables)
-  return Promise.resolve({ image, webWorker: worker })
 }
 
 export default readImageDICOMFileSeries
