e40caf088eb3f7e01d66a978c4f45ca7ed6dc0e3
Matt McCormick <matt.mccormick@kitware.com>
2020-05-13 14:59:00 -0400

    doc(UnpkgIOExample): Add polyData support, updates

--- examples/UnpkgIO/src/index.js
+++ examples/UnpkgIO/src/index.js
@@ -2,12 +2,18 @@ import readFile from 'itk/readFile'
 import curry from 'curry'
 
 const outputFileInformation = curry(function outputFileInformation (outputTextArea, event) {
+  outputTextArea.textContent = "Loading...";
+
   const dataTransfer = event.dataTransfer
   const files = event.target.files || dataTransfer.files
+
+  const viewerElement = document.getElementById('viewer')
+  !!viewerElement && itkVtkViewer.createViewerFromFiles(viewerElement, files)
+
   return readFile(null, files[0])
-    .then(function ({ image, mesh, webWorker }) {
+    .then(function ({ image, mesh, polyData, webWorker }) {
       webWorker.terminate()
-      const imageOrMesh = image || mesh
+      const imageOrMeshOrPolyData = image || mesh || polyData;
 
       function replacer (key, value) {
         if (!!value && value.byteLength !== undefined) {
@@ -15,7 +21,7 @@ const outputFileInformation = curry(function outputFileInformation (outputTextAr
         }
         return value
       }
-      outputTextArea.textContent = JSON.stringify(imageOrMesh, replacer, 4)
+      outputTextArea.textContent = JSON.stringify(imageOrMeshOrPolyData, replacer, 4);
     })
 })
 
