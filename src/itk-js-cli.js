edf816ca71b45c142ef8d8474f6817582fe02769
Matt McCormick <matt.mccormick@kitware.com>
2020-09-29 14:23:57 -0400

    feat(itk-js-cli): Update default Docker image for 13.1.4

--- src/itk-js-cli.js
+++ src/itk-js-cli.js
@@ -38,7 +38,7 @@ const build = (sourceDir) => {
     process.exit(dockerVersion.status)
   }
 
-  let dockerImage = 'insighttoolkit/itk-js:20200518-2906428'
+  let dockerImage = 'insighttoolkit/itk-js:20200929-6488869'
   if (program.commands[0].image) {
     dockerImage = program.commands[0].image
   }
