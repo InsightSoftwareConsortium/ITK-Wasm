964fafeb2ab661b32b7f2b69e2e52d896c6d21ca
Matt McCormick <matt.mccormick@kitware.com>
2021-09-22 09:00:48 -0400

    feat(Typescript): Add createWebworkerPromise.ts

---
+++ src/itkConfig.ts
@@ -0,0 +1,13 @@
+//
+// itkModulesPath is the path that contains the directories
+//
+// - WebWorkers/
+// - ImageIOs/
+// - MeshIOs/
+// - PolyDataIOs/
+//
+const itkConfig = {
+  itkModulesPath: 'itk'
+}
+
+export default itkConfig
