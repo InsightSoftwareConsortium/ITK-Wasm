ed418cc3e43744a65458cffd6d63d92e5f81f6df
Matt McCormick <matt.mccormick@kitware.com>
2021-09-22 20:35:29 -0400

    feat(Typescript): Add loadEmscriptenModuleBrowser.ts

---
+++ src/core/internal/loadEmscriptenModuleBrowser.ts
@@ -0,0 +1,48 @@
+// Load the Emscripten module in the browser.
+//
+// If the browser supports WebAssembly, then use the path the the WebAssembly
+// wrapper module instead.
+//
+// If itkModulesPath is a relative Path, then resolve assuming we were called
+// from <itkModulesPath>/WebWorkers/, since modules are loaded by the web
+// workers.
+//
+// itkModulesPath is usually taken from './itkConfig', but a different value
+// could be passed.
+//
+// If isAbsoluteURL is `true`, then itkModulesPath is not used, and
+// pipelinePath is assumed to be an absoluteURL.
+//
+// modulesDirectory is one of "ImageIOs", "MeshIOs", or "Pipelines"
+//
+// pipelinePath is the file name of the emscripten module without the ".js"
+// extension
+function loadEmscriptenModule(itkModulesPath: string, modulesDirectory: string, pipelinePath: string, isAbsoluteURL: boolean): object {
+  let prefix = itkModulesPath
+  if (itkModulesPath[0] !== '/' && !itkModulesPath.startsWith('http')) {
+    prefix = '..'
+  }
+  const moduleScriptDir = prefix + '/' + modulesDirectory
+  if (typeof WebAssembly === 'object' && typeof WebAssembly.Memory === 'function') {
+    let modulePath = moduleScriptDir + '/' + pipelinePath + 'Wasm.js'
+    if (isAbsoluteURL) {
+      modulePath = pipelinePath + 'Wasm.js'
+    }
+    importScripts(modulePath)
+    const moduleBaseName: string = pipelinePath.replace(/.*\//, '')
+    // @ts-ignore: error TS7053: Element implicitly has an 'any' type because expression of type 'string' can't be used to index type 'WorkerGlobalScope & typeof globalThis'.
+    const wrapperModule = self[moduleBaseName] as (moduleParams: object) => object
+    const emscriptenModule: object = wrapperModule({ moduleScriptDir, isAbsoluteURL, pipelinePath });
+    return emscriptenModule;
+  } else {
+    let modulePath = moduleScriptDir + '/' + pipelinePath + '.js'
+    if (isAbsoluteURL) {
+      modulePath = pipelinePath + '.js'
+    }
+    importScripts(modulePath)
+    // @ts-ignore: error TS2552: Cannot find name 'Module'
+    return Module
+  }
+}
+
+export default loadEmscriptenModule
