9437cc0a4e5cad810ea16433d88d57705977e827
Matt McCormick <matt.mccormick@kitware.com>
2021-09-27 16:10:05 -0400

    feat(Typescript): Add readPolyDataLocalFile.ts

---
+++ src/io/readPolyDataLocalFile.ts
@@ -0,0 +1,54 @@
+import path from 'path'
+import mime from 'mime-types'
+import fs from 'fs'
+
+import mimeToIO from './internal/MimeToPolyDataIO.js'
+import getFileExtension from './getFileExtension.js'
+import extensionToIO from './internal/extensionToPolyDataIO.js'
+import IOTypes from '../core/IOTypes.js'
+import PolyData from '../core/vtkPolyData.js'
+
+import loadEmscriptenModule from '../core/internal/loadEmscriptenModuleNode.js'
+import runPipelineEmscripten from '../core/internal/runPipelineEmscripten.js'
+import CLIEmscriptenModule from '../core/CLIEmscriptenModule.js'
+
+function readPolyDataLocalFile(filePath: string): Promise<PolyData> {
+  return new Promise(function (resolve, reject) {
+    const polyDataIOsPath = path.resolve(__dirname, 'PolyDataIOs')
+    const absoluteFilePath = path.resolve(filePath)
+    const filePathBasename = path.basename(filePath)
+    try {
+      const mimeType = mime.lookup(absoluteFilePath)
+      const extension = getFileExtension(absoluteFilePath)
+
+      let io = null
+      if (mimeToIO.has(mimeType)) {
+        io = mimeToIO.get(mimeType)
+      } else if (extensionToIO.has(extension)) {
+        io = extensionToIO.get(extension)
+      }
+      if (io === null) {
+        reject(Error('Could not find IO for: ' + absoluteFilePath))
+      }
+
+      const modulePath = path.join(polyDataIOsPath, io as string)
+      const Module = loadEmscriptenModule(modulePath) as CLIEmscriptenModule
+      const fileContents = new Uint8Array(fs.readFileSync(absoluteFilePath))
+
+      const args = [filePathBasename, filePathBasename + '.output.json']
+      const desiredOutputs = [
+        { path: args[1], type: IOTypes.vtkPolyData }
+      ]
+      const inputs = [
+        { path: args[0], type: IOTypes.Binary, data: fileContents }
+      ]
+      const { outputs } = runPipelineEmscripten(Module, args, desiredOutputs, inputs)
+
+      resolve(outputs[0].data as PolyData)
+    } catch (err) {
+      reject(err)
+    }
+  })
+}
+
+export default readPolyDataLocalFile
