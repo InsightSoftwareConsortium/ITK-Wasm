fe13161909ec523a49d594fc9e3f5c03b964fe3c
Matt McCormick <matt.mccormick@kitware.com>
2021-10-05 22:38:40 -0400

    test(MGH): Move and update imports

--- test/node/io/MGHTest.js
+++ test/node/io/MGHTest.js
@@ -1,13 +1,10 @@
-const test = require('ava')
-const path = require('path')
+import test from 'ava'
+import path from 'path'
 
-const IntTypes = require(path.resolve(__dirname, '..', 'dist', 'IntTypes.js'))
-const PixelTypes = require(path.resolve(__dirname, '..', 'dist', 'PixelTypes.js'))
-const readImageLocalFile = require(path.resolve(__dirname, '..', 'dist', 'readImageLocalFile.js'))
-const writeImageLocalFile = require(path.resolve(__dirname, '..', 'dist', 'writeImageLocalFile.js'))
+import { IntTypes, PixelTypes, readImageLocalFile, writeImageLocalFile } from '../../../dist/index.js'
 
-const testInputFilePath = path.resolve(__dirname, '..', 'build', 'ExternalData', 'test', 'Input', 'T1.mgz')
-const testOutputFilePath = path.resolve(__dirname, '..', 'build', 'Testing', 'Temporary', 'MGHTest-T1.mgz')
+const testInputFilePath = path.resolve('build', 'ExternalData', 'test', 'Input', 'T1.mgz')
+const testOutputFilePath = path.resolve('build', 'Testing', 'Temporary', 'MGHTest-T1.mgz')
 
 const verifyImage = (t, image) => {
   t.is(image.imageType.dimension, 3, 'dimension')
