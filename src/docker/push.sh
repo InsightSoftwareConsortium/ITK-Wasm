0602f4da25e719351f74282ba43671f183f19b89
Matt McCormick <matt.mccormick@kitware.com>
2021-10-22 10:33:41 -0400

    build(push.sh): Enable kitware/itk-wasm-vtk push

--- src/docker/push.sh
+++ src/docker/push.sh
@@ -5,11 +5,11 @@ set -eo pipefail
 TAG=$(date '+%Y%m%d')-$(git rev-parse --short HEAD)
 
 # These must be set before calling the script
-#echo $DOCKERHUB_KITWARE_PASSWORD | docker login --username "$DOCKERHUB_KITWARE_USERNAME" --password-stdin
+echo $DOCKERHUB_KITWARE_PASSWORD | docker login --username "$DOCKERHUB_KITWARE_USERNAME" --password-stdin
 
-#docker push kitware/itk-wasm-vtk:${TAG}-debug
-#docker push kitware/itk-wasm-vtk:${TAG}
-#docker push kitware/itk-wasm-vtk:latest
+docker push kitware/itk-wasm-vtk:${TAG}-debug
+docker push kitware/itk-wasm-vtk:${TAG}
+docker push kitware/itk-wasm-vtk:latest
 
 echo $DOCKERHUB_INSIGHTTOOLKIT_PASSWORD | docker login --username "$DOCKERHUB_INSIGHTTOOLKIT_USERNAME" --password-stdin
 
