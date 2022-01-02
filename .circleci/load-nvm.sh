6bde9e5324a60f7fbafc6f946365f07e8b2d6dae
Matt McCormick <matt.mccormick@kitware.com>
2022-01-01 21:54:33 -0500

    ci(CircleCI): Remove -- replaced by GitHub Actions
    Closes #398

--- .circleci/load-nvm.sh
+++
@@ -1,4 +0,0 @@
-# load-nvm.sh
-#curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-export NVM_DIR="/opt/circleci/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
