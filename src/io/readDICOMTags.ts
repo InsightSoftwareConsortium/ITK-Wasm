5f47b43bbe740525c270048e1625042dbe950c21
Matt McCormick <matt.mccormick@kitware.com>
2022-02-24 23:28:35 -0500

    feat(readDICOMTagsArrayBuffer): Initial addition

--- src/io/readDICOMTags.ts
+++ src/io/readDICOMTags.ts
@@ -1,59 +1,11 @@
-import createWebWorkerPromise from '../core/internal/createWebWorkerPromise.js'
 import { readAsArrayBuffer } from 'promise-file-reader'
-import PipelineInput from '../pipeline/PipelineInput.js'
-import TextStream from '../core/TextStream.js'
-
-import config from '../itkConfig.js'
 
 import ReadDICOMTagsResult from './ReadDICOMTagsResult.js'
-import InterfaceTypes from '../core/InterfaceTypes.js'
+import readDICOMTagsArrayBuffer from './readDICOMTagsArrayBuffer.js'
 
 async function readDICOMTags (webWorker: Worker, file: File, tags: string[] | null = null): Promise<ReadDICOMTagsResult> {
-  let worker = webWorker
-  const { webworkerPromise, worker: usedWorker } = await createWebWorkerPromise(
-    'pipeline',
-    worker
-  )
-  worker = usedWorker
-
   const arrayBuffer = await readAsArrayBuffer(file)
-  const dataArray = new Uint8Array(arrayBuffer)
-
-  const path = `./${file.name}`
-  const args = [path, '0', '--memory-io']
-  const inputs = [
-    { type: InterfaceTypes.BinaryFile, data: { data: dataArray, path } }
-  ] as PipelineInput[]
-  if (tags != null) {
-    args.push('--tags-to-read')
-    args.push('1')
-    inputs.push({ type: InterfaceTypes.TextStream, data: { data: JSON.stringify({ tags: tags }) } })
-  }
-  const outputs = [
-    { type: InterfaceTypes.TextStream }
-  ]
-
-  interface PipelineResult {
-    stdout: string
-    stderr: string
-    outputs: any[]
-  }
-
-  const result: PipelineResult = await webworkerPromise.postMessage(
-    {
-      operation: 'readDICOMTags',
-      config: config,
-      pipelinePath: 'ReadDICOMTags', // placeholder
-      args,
-      outputs,
-      inputs
-    },
-    [arrayBuffer]
-  )
-  const tagsJSON = (result.outputs[0].data as TextStream).data
-  const tagsResult = JSON.parse(tagsJSON)
-  const tagsMap: Map<string, string> = new Map(tagsResult.tags)
-  return { tags: tagsMap, webWorker: worker }
+  return await readDICOMTagsArrayBuffer(webWorker, arrayBuffer, tags)
 }
 
 export default readDICOMTags
