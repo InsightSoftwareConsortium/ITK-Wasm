f96866a5e9cc8699042982a02de0a42eb0b6d0d3
Matt McCormick <matt.mccormick@kitware.com>
2022-03-16 11:23:44 -0400

    refactor(createWebWorkerPromise): Use pipelineWorkerUrl with full path to worker
    Use itkConfig.pipelineWorkerUrl with the full path to the worker bundle instead
    of itkConfig.webWorkersUrl. We now only need one worker,
    *pipeline-worker.js*. This makes it easier to specified the non-minified
    worker bundle for debugging.
    
    Also, add experimental suppport for using a worker packaged by the
    bundler, e.g. WebPack 5, by setting itkConfig.pipelineWorkerUrl to null.

--- test/browser/config/itkConfigBrowserTest.js
+++ test/browser/config/itkConfigBrowserTest.js
@@ -1,5 +1,5 @@
 const itkConfig = {
-  webWorkersUrl: __BASE_PATH__ + '/dist/web-workers', // eslint-disable-line no-undef
+  pipelineWorkerUrl: __BASE_PATH__ + '/dist/web-workers/bundles/pipeline.worker.js', // eslint-disable-line no-undef
   imageIOUrl: __BASE_PATH__ + '/dist/image-io', // eslint-disable-line no-undef
   meshIOUrl: __BASE_PATH__ + '/dist/mesh-io', // eslint-disable-line no-undef
   pipelinesUrl: __BASE_PATH__ + '/dist/pipeline' // eslint-disable-line no-undef
