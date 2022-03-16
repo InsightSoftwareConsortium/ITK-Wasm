f50e6f52f01ece9a86246e58512d1bebe87e9ed3
Matt McCormick <matt.mccormick@kitware.com>
2022-03-15 23:34:54 -0400

    test(WebpackExample): Use cypress

--- examples/Webpack/test/config/itkConfigTest.js
+++
@@ -1,8 +0,0 @@
-const itkConfig = {
-  webWorkersUrl: __BASE_PATH__ + '/dist/itk/web-workers',
-  imageIOUrl: __BASE_PATH__ + '/dist/itk/image-io',
-  meshIOUrl: __BASE_PATH__ + '/dist/itk/mesh-io',
-  pipelinesUrl: __BASE_PATH__ + '/dist/itk/pipelines',
-}
-
-export default itkConfig
