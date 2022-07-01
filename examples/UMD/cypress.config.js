313566a7aa7ec29b55cd77ec2a5d25b929a0e309
Matt McCormick <matt.mccormick@kitware.com>
2022-07-01 17:11:22 -0400

    test(UMDExample): upgrade to Cypress 10

---
+++ examples/UMD/cypress.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  e2e: {
+    defaultCommandTimeout: 8000,
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+  },
+})
