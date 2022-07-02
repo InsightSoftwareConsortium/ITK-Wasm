12928f9c7bf6d229b72b2fcaf242b298545a8cfe
Matt McCormick <matt.mccormick@kitware.com>
2022-07-02 11:41:13 -0400

    ci(UMDExample): update for Cypress 10

--- examples/UMD/cypress.config.js
+++ examples/UMD/cypress.config.js
@@ -6,7 +6,7 @@ module.exports = defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+      // implement node event listeners here
     },
   },
 })
