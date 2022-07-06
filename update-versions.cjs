d70d7b3143439f5b26e7e148b381cb627671c83c
Forrest Li <forrest.li@kitware.com>
2022-07-06 11:26:13 -0400

    chore: publish io packages

---
+++ update-versions.cjs
@@ -0,0 +1,25 @@
+const fs = require('fs')
+
+const version = process.argv[2]
+if (!version) {
+  console.error(`usage: ${__filename} VERSION`)
+  process.exit(1)
+}
+
+function rewriteVersion(filename) {
+  if (!fs.existsSync(filename)) {
+    return
+  }
+  const lines = fs.readFileSync(filename, { encoding: 'utf-8' }).split('\n')
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i]
+    if (lines[i].startsWith('const version = ')) {
+      lines[i] = `const version = '${version}';`
+    }
+  }
+
+  fs.writeFileSync(filename, lines.join('\n'))
+}
+
+rewriteVersion('dist/itkConfig.js')
+rewriteVersion('dist/itkConfigDevelopment.js')
