8dde82ea8946ced47fea93e1149aab1cde08c9b2
Matt McCormick <matt.mccormick@kitware.com>
2022-07-10 20:06:12 -0400

    build(itk-wasm): bump for itk-5.3rc4.post1

--- src/python/itkwasm/__init__.py
+++ src/python/itkwasm/__init__.py
@@ -1,6 +1,6 @@
 """itkwasm: Python interface to itk-wasm WebAssembly modules."""
 
-__version__ = "1.0b0"
+__version__ = "1.0b1"
 
 from .image import Image, ImageType
 from .mesh import Mesh, MeshType
