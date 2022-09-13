e8cf50cf314a3f0efba43bcc739ea3cdae7ca691
Matt McCormick <matt.mccormick@kitware.com>
2022-09-13 11:30:40 -0400

    fix(bindings): Pass pipeline path for generation

--- src/interfaceJSONNode.js
+++ src/interfaceJSONNode.js
@@ -2,7 +2,7 @@ import path from 'path'
 
 import { runPipelineNode } from '../dist/node/index.js'
 
-const pipelinePath = path.resolve('./public/pipelines/structured-report-to-text')
+const pipelinePath = path.resolve(process.argv[2])
 const { returnValue, stdout, stderr } = await runPipelineNode(pipelinePath, ['--interface-json'])
 process.exit(returnValue)
 
