1be234a4852e751f09df12f9c7c39b1957b9d738
Shreeraj Jadhav <shreerajjadhav@gmail.com>
2022-09-22 23:21:32 -0400

    fix(itk-wasm-cli): enforce alpha-numeric parameter names
    Change function camelCase() to additionally remove non-alphanumeric chars.
    Also throw error if the final string is empty (sanitization for parameter names).

--- src/core/internal/camelCase.ts
+++ src/core/internal/camelCase.ts
@@ -1,7 +1,17 @@
 function camelCase (kebobCase: string): string {
-  return kebobCase.replace(/-([a-z])/g, (kk) => {
-    return kk[1].toUpperCase()
-  })
+  // make any alphabets that follows '-' an uppercase character, and remove the corresponding hyphen
+  let cameledParam = kebobCase.replace(/-([a-z])/g, (kk) => {
+    return kk[1].toUpperCase();
+  });
+
+  // remove all non-alphanumeric characters
+  const outParam = cameledParam.replace(/([^0-9a-z])/ig, '')
+
+  // check if resulting string is empty
+  if(outParam === '') {
+    console.error(`Resulting string is empty.`)
+  }
+  return outParam
 }
 
 export default camelCase
