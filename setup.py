06916b20d7ed3243609e45cdbdd9cbb05b5bbca6
Matt McCormick <matt.mccormick@kitware.com>
2022-12-19 16:18:44 -0500

    docs(NativePythonPackage): Improve long description

--- setup.py
+++ setup.py
@@ -12,14 +12,14 @@ except ImportError:
 
 setup(
     name='itk-webassemblyinterface',
-    version='1.0b52',
+    version='1.0b53',
     author='Insight Software Consortium',
     author_email='itk+community@discourse.itk.org',
     packages=['itk'],
     package_dir={'itk': 'itk'},
     download_url=r'https://github.com/InsightSoftwareConsortium/itk-wasm',
     description=r'IO with the itk-wasm file formats',
-    long_description='ITK is an open-source, cross-platform library that provides developers with an extensive suite of software tools for image analysis. Developed through extreme programming methodologies, ITK employs leading-edge algorithms for registering and segmenting multidimensional scientific images.',
+    long_description='This package provides native binary interfaces to the itk-wasm file formats that can be used with itk.imread, itk.meshread, itk.transformread, itk.imwrite, etc. For execution of itk-wasm WASI binaries see itkwasm and itkwasm-* packages.',
     classifiers=[
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python",
