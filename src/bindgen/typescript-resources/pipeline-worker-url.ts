d2c275e647c9ee2eda8c58787fa2749aa34023c4
Matt McCormick <matt.mccormick@kitware.com>
2023-01-24 12:49:17 -0500

    build(typescriptResources): Ignore unloadable / unfound package.json
    This file is not expected to exist.

--- src/bindgen/typescript-resources/pipeline-worker-url.ts
+++ src/bindgen/typescript-resources/pipeline-worker-url.ts
@@ -1,5 +1,6 @@
+// @ts-expect-error error TS2732: Cannot find module '../package.json'. Consider using '--resolveJsonModule' to import module with '.json' extension.
 import packageJson from '../package.json'
-let pipelineWorkerUrl: string | URL | null = `https://cdn.jsdelivr.net/npm/<bindgenPackageName>@${packageJson.version}/dist/web-workers/pipeline.worker.js`
+let pipelineWorkerUrl: string | URL | null = `https://cdn.jsdelivr.net/npm/<bindgenPackageName>@${packageJson.version as string}/dist/web-workers/pipeline.worker.js`
 
 export function setPipelineWorkerUrl (workerUrl: string | URL | null): void {
   pipelineWorkerUrl = workerUrl
