f100e5cac1ed2d7657a13b24e9deff13fac7165b
Matt McCormick <matt.mccormick@kitware.com>
2023-02-13 18:04:50 -0500

    feat(itk-wasm-cli): Update default Docker image for 20230213-bd821f33

--- src/itk-wasm-cli.js
+++ src/itk-wasm-cli.js
@@ -12,7 +12,7 @@ import typescriptBindgen from './bindgen/typescript.js'
 const program = new Command()
 
 // Array of types that will require an import from itk-wasm
-const defaultImageTag = '20230213-08fa7e35'
+const defaultImageTag = '20230213-bd821f33'
 
 function processCommonOptions() {
   const options = program.opts()
