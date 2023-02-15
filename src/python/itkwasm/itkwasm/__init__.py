81789a3ca35cce8f0f6f73b67af7b0ba3927c0c3
Matt McCormick <matt.mccormick@kitware.com>
2023-02-15 07:08:32 -0500

    chore(Python): Bump Python package to 1.0b74

--- src/python/itkwasm/itkwasm/__init__.py
+++ src/python/itkwasm/itkwasm/__init__.py
@@ -1,6 +1,6 @@
 """itkwasm: Python interface to itk-wasm WebAssembly modules."""
 
-__version__ = "1.0b3"
+__version__ = "1.0b74"
 
 from .interface_types import InterfaceTypes
 from .image import Image, ImageType
