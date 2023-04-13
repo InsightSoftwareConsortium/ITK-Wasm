2284f7f8a0f646db81168b0b33dbc7817b140898
Matt McCormick <matt.mccormick@kitware.com>
2023-04-12 23:05:50 -0400

    docs(Sphinx): Add pydata-sphinx-theme, enable napolean theme

--- packages/core/python/itkwasm/docs/conf.py
+++ packages/core/python/itkwasm/docs/conf.py
@@ -7,13 +7,13 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'itkwasm'
-copyright = '2023, Matt McCormick'
+copyright = '2023, NumFOCUS'
 author = 'Matt McCormick'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx.ext.napoleon', 'sphinx.ext.autodoc']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -23,5 +23,17 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'pydata_sphinx_theme'
 html_static_path = ['_static']
+
+html_theme_options = {
+    "use_edit_page_button": True,
+    "github_url": "https://github.com/InsightSoftwareConsortium/itk-wasm",
+}
+
+html_context = {
+    "github_user": "InsightSoftwareConsortium",
+    "github_repo": "itk-wasm",
+    "github_version": "main",
+    "doc_path": "packages/core/python/itkwasm/docs",
+}
