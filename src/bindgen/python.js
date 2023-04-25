66a9ccb8e59a403a3a4cb4cbb125fb3be57a86e2
Matt McCormick <matt.mccormick@kitware.com>
2023-04-25 08:47:33 -0400

    feat(bindgen): Use __version__ for Python JS package version

--- src/bindgen/python.js
+++ src/bindgen/python.js
@@ -684,11 +684,13 @@ function emscriptenPyodideModule(packageDir, pypackage, options) {
   const defaultJsModuleName = options.packageName.replace('itkwasm-', '')
   const version = options.packageVersion ?? '0.1.0'
 
-  const moduleUrl = options.jsModuleUrl ?? `https://cdn.jsdelivr.net/npm/${defaultJsPackageName}@${version}/dist/bundles/${defaultJsModuleName}.js`
+  const moduleUrl = options.jsModuleUrl ?? `https://cdn.jsdelivr.net/npm/${defaultJsPackageName}@{__version__}/dist/bundles/${defaultJsModuleName}.js`
 
   const moduleContent = `from itkwasm.pyodide import JsPackageConfig, JsPackage
 
-default_config = JsPackageConfig("${moduleUrl}")
+from ._version import __version__
+
+default_config = JsPackageConfig(f"${moduleUrl}")
 js_package = JsPackage(default_config)
 `
 
