be51ceaa0ed856db01a7d1a590eb9f205782237d
Matt McCormick <matt.mccormick@kitware.com>
2023-04-24 22:51:13 -0400

    build(compress-stringify): Bump to 0.4.3

--- packages/compress-stringify/typescript/src/pipeline-worker-url.ts
+++ packages/compress-stringify/typescript/src/pipeline-worker-url.ts
@@ -1,9 +1,8 @@
-// @ts-ignore: TS2305: Module '"itk-wasm"' has no exported member 'getPipelineWorkerUrl'.
 import { getPipelineWorkerUrl as itkWasmGetPipelineWorkerUrl } from 'itk-wasm'
 import packageJson from '../package.json'
 
 let pipelineWorkerUrl: string | URL | null | undefined
-let defaultPipelineWorkerUrl: string | URL | null = `https://cdn.jsdelivr.net/npm/@itk-wasm/compress-stringify@${packageJson.version}/dist/web-workers/pipeline.worker.js`
+const defaultPipelineWorkerUrl = `https://cdn.jsdelivr.net/npm/@itk-wasm/compress-stringify@${packageJson.version}/dist/web-workers/pipeline.worker.js`
 
 export function setPipelineWorkerUrl (workerUrl: string | URL | null): void {
   pipelineWorkerUrl = workerUrl
--- packages/compress-stringify/typescript/src/pipelines-base-url.ts
+++ packages/compress-stringify/typescript/src/pipelines-base-url.ts
@@ -1,9 +1,8 @@
-// @ts-ignore: TS2305: Module '"itk-wasm"' has no exported member 'getPipelinesBaseUrl'.
 import { getPipelinesBaseUrl as itkWasmGetPipelinesBaseUrl } from 'itk-wasm'
 import packageJson from '../package.json'
 
 let pipelinesBaseUrl: string | URL | undefined
-const defaultPipelinesBaseUrl = `https://cdn.jsdelivr.net/npm/@itk-wasm/compress-stringify@${packageJson.version}/dist/pipelines`
+let defaultPipelinesBaseUrl: string | URL = `https://cdn.jsdelivr.net/npm/@itk-wasm/compress-stringify@${packageJson.version}/dist/pipelines`
 
 export function setPipelinesBaseUrl (baseUrl: string | URL): void {
   pipelinesBaseUrl = baseUrl
