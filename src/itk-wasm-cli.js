f4d391c065316fd57a078f4ee4b57a6ace62f155
Matt McCormick <matt.mccormick@kitware.com>
2023-05-26 11:53:37 -0400

    feat(itk-wasm-cli): Update default Docker image for 20230526-8ae95dcd

--- src/itk-wasm-cli.js
+++ src/itk-wasm-cli.js
@@ -13,7 +13,7 @@ import pythonBindgen from './bindgen/python.js'
 const program = new Command()
 
 // Array of types that will require an import from itk-wasm
-const defaultImageTag = '20230525-bfee36f8'
+const defaultImageTag = '20230526-8ae95dcd'
 
 function processCommonOptions(wasiDefault=false) {
   const options = program.opts()
