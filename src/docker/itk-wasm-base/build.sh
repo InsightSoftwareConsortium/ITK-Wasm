bfee36f81702e674b8d64a015b1a1864fdfdf098
Matt McCormick <matt.mccormick@kitware.com>
2023-05-25 23:39:41 -0400

    perf(WASI): Enable -msimd128

--- src/docker/itk-wasm-base/build.sh
+++ src/docker/itk-wasm-base/build.sh
@@ -26,7 +26,7 @@ done
 set -- "${newparams[@]}"  # overwrites the original positional params
 
 wasi_ld_flags="-flto -lwasi-emulated-process-clocks -lwasi-emulated-signal -lc-printscan-long-double"
-wasi_c_flags="-flto -D_WASI_EMULATED_PROCESS_CLOCKS -D_WASI_EMULATED_SIGNAL"
+wasi_c_flags="-flto -msimd128 -D_WASI_EMULATED_PROCESS_CLOCKS -D_WASI_EMULATED_SIGNAL"
 
 emscripten_debug_ld_flags="-fno-lto -s ALLOW_MEMORY_GROWTH=1"
 emscripten_debug_c_flags="-fno-lto -Wno-warn-absolute-paths"
