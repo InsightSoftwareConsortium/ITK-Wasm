dcc84e2f00d5042dbe50102a0f8193a86decaa02
Curtis Rueden <ctrueden@wisc.edu>
2023-06-07 13:53:06 +0200

    Translate some Python classes to Java
    Thanks ChatGPT for the kickstart.

---
+++ packages/core/java/src/main/java/org/itk/wasm/InterfaceTypes.java
@@ -0,0 +1,40 @@
+/*-
+ * #%L
+ * Java bindings for itk-wasm.
+ * %%
+ * Copyright (C) 2023 ITK developers.
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package org.itk.wasm;
+public enum InterfaceTypes {
+    TextFile("InterfaceTextFile"),
+    BinaryFile("InterfaceBinaryFile"),
+    TextStream("InterfaceTextStream"),
+    BinaryStream("InterfaceBinaryStream"),
+    Image("InterfaceImage"),
+    Mesh("InterfaceMesh"),
+    PolyData("InterfacePolyData"),
+    JsonObject("InterfaceJsonObject");
+
+    private String value;
+
+    private InterfaceTypes(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
