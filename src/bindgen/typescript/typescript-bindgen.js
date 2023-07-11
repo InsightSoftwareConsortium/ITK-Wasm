0f9969605f22de5fdc6eafcc65016fc3951b735f
Matt McCormick <matt.mccormick@kitware.com>
2023-07-11 18:38:44 -0400

    refactor(bindgen): factor output-demo-html.js

--- src/bindgen/typescript/typescript-bindgen.js
+++ src/bindgen/typescript/typescript-bindgen.js
@@ -5,54 +5,12 @@ import { markdownTable } from 'markdown-table'
 import wasmBinaryInterfaceJson from '../wasm-binary-interface-json.js'
 import interfaceJsonTypeToInterfaceType from '../interface-json-type-to-interface-type.js'
 import camelCase from '../camel-case.js'
+
 import interfaceJsonTypeToTypeScriptType from './interface-json-type-to-typescript-type.js'
 import packageToBundleName from './package-to-bundle-name.js'
 import inputParametersDemoHtml from './input-parameters-demo-html.js'
 import writeIfOverrideNotPresent from './write-if-override-not-present.js'
-
-function outputDemoHtml(prefix, indent, parameter) {
-  let result = ''
-  switch(parameter.type) {
-    case 'OUTPUT_TEXT_FILE:FILE':
-    case 'OUTPUT_TEXT_STREAM':
-      result += `${prefix}${indent}<sl-textarea disabled name="${parameter.name}" label="${camelCase(parameter.name)}" help-text="${parameter.description}"></sl-textarea>\n`
-      result += `${prefix}${indent}<sl-button variant="neutral" outline name="${parameter.name}-download" disabled>Download</sl-button>\n`
-      result += `<br /><br />\n`
-      break
-    case 'OUTPUT_BINARY_FILE:FILE':
-    case 'OUTPUT_BINARY_STREAM':
-      result += `${prefix}${indent}<sl-textarea name="${parameter.name}" label="${camelCase(parameter.name)}" help-text="${parameter.description}"><sl-skeleton effect="none"></sl-skeleton></sl-textarea>\n`
-      result += `${prefix}${indent}<sl-button variant="neutral" outline name="${parameter.name}-download" disabled>Download</sl-button>\n`
-      result += `<br /><br />\n`
-      break
-    case 'TEXT':
-      result += `${prefix}${indent}<sl-textarea disabled name="${parameter.name}" label="${camelCase(parameter.name)}" help-text="${parameter.description}"></sl-textarea>\n`
-      break
-    case 'INT':
-    case 'UINT':
-      if (parameter.itemsExpected !== 1 || parameter.itemsExpectedMin !== 1 || parameter.itemsExpectedMax !== 1) {
-        // TODO
-        console.error(`INT items != 1 are currently not supported`)
-        process.exit(1)
-      }
-      result += `${prefix}${indent}<sl-input disabled name="${parameter.name}" type="number" value="${parameter.default}" label="${camelCase(parameter.name)}" help-text="${parameter.description}"></sl-input>\n`
-      result += `<br />\n`
-      break
-    case 'BOOL':
-      result += `${prefix}${indent}<sl-checkbox disabled name="${parameter.name}">${camelCase(parameter.name)} - <i>${parameter.description}</i></sl-checkbox>\n`
-      result += `<br /><br />\n`
-      break
-    case 'OUTPUT_JSON':
-      result += `${prefix}${indent}<sl-tree ><sl-tree-item>${camelCase(parameter.name)} - <i>${parameter.description}</i></sl-tree-item></sl-tree>\n`
-      result += `${prefix}${indent}<sl-button variant="neutral" outline name="${parameter.name}-download" disabled>${camelCase(parameter.name)}</sl-button>\n`
-      result += `<br /><br />\n`
-      break
-    default:
-      console.error(`Unexpected interface type: ${parameter.type}`)
-      process.exit(1)
-  }
-  return result
-}
+import outputDemoHtml from './output-demo-html.js'
 
 function outputDemoRunTypeScript(functionName, prefix, indent, parameter) {
   const parameterName = camelCase(parameter.name)
