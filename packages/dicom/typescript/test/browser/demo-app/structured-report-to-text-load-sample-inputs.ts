059940d055e949d2b2e56ef73e172e1587a4e1b0
Matt McCormick <matt.mccormick@kitware.com>
2023-08-03 21:05:54 -0400

    test(dicom): Add structured-report-to-text-load-sample-inputs.ts

--- packages/dicom/typescript/test/browser/demo-app/structured-report-to-text-load-sample-inputs.ts
+++ packages/dicom/typescript/test/browser/demo-app/structured-report-to-text-load-sample-inputs.ts
@@ -1,21 +1,11 @@
-// Generated file. To retain edits, remove this comment.
-
-export default null
-// export default function structuredReportToTextLoadSampleInputs (model) {
-
-  // Load sample inputs for the structuredReportToText function.
-  //
-  // This function should load sample inputs:
-  //
-  //  1) In the provided model map.
-  //  2) Into the corresponding HTML input elements.
-  //
-  // Example for an input named `exampleInput`:
-
-  // const exampleInput = 5
-  // model.inputs.set("exampleInput", exampleInput)
-  // const exampleElement = document.querySelector("#structuredReportToTextInputs [name=example-input]")
-  // exampleElement.value = 5
-
-  // return model
-// }
+export default async function structuredReportToTextLoadSampleInputs(model) {
+  const dicomButton = document.querySelector('#structuredReportToTextInputs sl-button[name=dicom-file-file-button]')
+  dicomButton.loading = true
+  const fileName = '88.33-comprehensive-SR.dcm'
+  const dicomReponse = await fetch(`https://bafybeihmnqsufckyjxt2z3yunppqggtq2rle7fta27rbetmf7fgviytghi.ipfs.w3s.link/ipfs/bafybeihmnqsufckyjxt2z3yunppqggtq2rle7fta27rbetmf7fgviytghi/input/${fileName}`)
+  const dicomData = new Uint8Array(await dicomReponse.arrayBuffer())
+  model.inputs.set('dicomFile', { data: dicomData, path: fileName })
+  const dicomElement = document.querySelector('#structuredReportToTextInputs  sl-input[name=dicom-file]')
+  dicomElement.value = dicomData.subarray(0, 50).toString()
+  dicomButton.loading = false
+}
