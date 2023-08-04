2b1df8aba284c4b6b970250cc61f8eb2c693cda6
Matt McCormick <matt.mccormick@kitware.com>
2023-08-04 11:34:29 -0400

    docs(dicom): Update breaking changes in pipeline migration
    And remove outdated tests that are already covered.

---
+++ test/browser/io/DICOMTest.js
@@ -0,0 +1,44 @@
+import test from 'tape'
+import axios from 'axios'
+
+import { IntTypes, PixelTypes, getMatrixElement, readImageFile, readDICOMTags, readDICOMTagsArrayBuffer } from 'browser/index.js'
+
+export default function () {
+  test('Test reading a DICOM file', t => {
+    const fileName = '1.3.6.1.4.1.5962.99.1.3814087073.479799962.1489872804257.100.0.dcm'
+    const testFilePath = 'base/build-emscripten/ExternalData/test/Input/' + fileName
+    return axios.get(testFilePath, { responseType: 'blob' }).then(function (response) {
+      const jsFile = new window.File([response.data], fileName)
+      return jsFile
+    })
+      .then(function (jsFile) {
+        return readImageFile(null, jsFile)
+      }).then(function ({ image, webWorker }) {
+        webWorker.terminate()
+        t.is(image.imageType.dimension, 3, 'dimension')
+        t.is(image.imageType.componentType, IntTypes.Int16, 'componentType')
+        t.is(image.imageType.pixelType, PixelTypes.Scalar, 'pixelType')
+        t.is(image.imageType.components, 1, 'components')
+        t.is(image.origin[0], -32.9551, 'origin[0]')
+        t.is(image.origin[1], -133.9286, 'origin[1]')
+        t.is(image.origin[2], 116.7857, 'origin[2]')
+        t.is(image.spacing[0], 1.0, 'spacing[0]')
+        t.is(image.spacing[1], 1.0, 'spacing[1]')
+        t.is(image.spacing[2], 1.0, 'spacing[2]')
+        t.is(getMatrixElement(image.direction, 3, 0, 0), 0.0, 'direction (0, 0)')
+        t.is(getMatrixElement(image.direction, 3, 0, 1), 0.0, 'direction (0, 1)')
+        t.is(getMatrixElement(image.direction, 3, 0, 2), -1.0, 'direction (0, 2)')
+        t.is(getMatrixElement(image.direction, 3, 1, 0), 1.0, 'direction (1, 0)')
+        t.is(getMatrixElement(image.direction, 3, 1, 1), 0.0, 'direction (1, 1)')
+        t.is(getMatrixElement(image.direction, 3, 1, 2), 0.0, 'direction (1, 2)')
+        t.is(getMatrixElement(image.direction, 3, 2, 0), 0.0, 'direction (2, 0)')
+        t.is(getMatrixElement(image.direction, 3, 2, 1), -1.0, 'direction (2, 1)')
+        t.is(getMatrixElement(image.direction, 3, 2, 2), 0.0, 'direction (2, 2)')
+        t.is(image.size[0], 256, 'size[0]')
+        t.is(image.size[1], 256, 'size[1]')
+        t.is(image.size[2], 1, 'size[2]')
+        t.is(image.data.length, 65536, 'data.length')
+        t.is(image.data[1000], 3, 'data[1000]')
+        t.end()
+      })
+  })
