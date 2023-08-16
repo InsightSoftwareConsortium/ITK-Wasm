4b960224ffb7743ded07567426e4494760851eb6
Paul Elliott <paul.elliott@kitware.com>
2023-08-16 10:04:51 -0400

    fix(read-dicom-tags): tweek typescript options and return types
    closes #896

--- packages/dicom/typescript/src/read-dicom-tags-options.ts
+++ packages/dicom/typescript/src/read-dicom-tags-options.ts
@@ -1,9 +1,6 @@
-// Generated file. To retain edits, remove this comment.
-
 interface ReadDicomTagsOptions {
   /** A JSON object with a "tags" array of the tags to read. If not provided, all tags are read. Example tag: "0008|103e". */
-  tagsToRead?: Object
-
+  tagsToRead?: { tags: Array<string> }
 }
 
 export default ReadDicomTagsOptions
