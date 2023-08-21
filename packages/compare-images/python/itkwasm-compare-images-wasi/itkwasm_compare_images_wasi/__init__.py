364e41d066cfc8395bf0558a478d411a123680f4
Matt McCormick <matt.mccormick@kitware.com>
2023-08-20 20:58:19 -0400

    chore(python): bump package dep version to 1.0b129

--- packages/compare-images/python/itkwasm-compare-images-wasi/itkwasm_compare_images_wasi/__init__.py
+++ packages/compare-images/python/itkwasm-compare-images-wasi/itkwasm_compare_images_wasi/__init__.py
@@ -1,5 +1,6 @@
 """itkwasm-compare-images-wasi: Read files and images related to compare-images file format. WASI implementation."""
 
 from .compare_double_images import compare_double_images
+from .compare_images import compare_images
 
 from ._version import __version__
