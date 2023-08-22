827e10c83dedcc35bb1ffa4bc72204c7b390bc8d
Matt McCormick <matt.mccormick@kitware.com>
2023-08-22 10:26:01 -0400

    feat(compare-images): add compare_images.py, compare_images_async.py to dispatch

--- packages/compare-images/python/itkwasm-compare-images/itkwasm_compare_images/__init__.py
+++ packages/compare-images/python/itkwasm-compare-images/itkwasm_compare_images/__init__.py
@@ -2,6 +2,8 @@
 
 from .compare_double_images_async import compare_double_images_async
 from .compare_double_images import compare_double_images
+from .compare_images_async import compare_images_async
+from .compare_images import compare_images
 from .vector_magnitude_async import vector_magnitude_async
 from .vector_magnitude import vector_magnitude
 
