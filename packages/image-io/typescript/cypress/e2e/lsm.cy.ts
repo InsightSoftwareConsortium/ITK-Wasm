1926841d2b5e91992d2f770519138ccbe3a71395
Matt McCormick <matt.mccormick@kitware.com>
2023-10-07 08:57:16 -0400

    test(image-io): port LSM test to package

---
+++ packages/image-io/typescript/cypress/e2e/lsm.cy.ts
@@ -0,0 +1,29 @@
+import { demoServer } from './common.ts'
+
+describe('lsm', () => {
+  beforeEach(function() {
+    cy.visit(demoServer)
+
+    const testPathPrefix = '../test/data/input/'
+
+    const testImageFiles = [
+      'cthead1.lsm',
+    ]
+    testImageFiles.forEach((fileName) => {
+      cy.readFile(`${testPathPrefix}${fileName}`, null).as(fileName)
+    })
+  })
+
+  it('Reads a lsm image', function () {
+    cy.get('sl-tab[panel="lsmReadImage-panel"]').click()
+
+    const testFile = { contents: new Uint8Array(this['cthead1.lsm']), fileName: 'cthead1.lsm' }
+    cy.get('#lsmReadImageInputs input[name="serialized-image-file"]').selectFile([testFile,], { force: true })
+    cy.get('#lsmReadImage-serialized-image-details').should('contain', '73,73')
+
+    cy.get('#lsmReadImageInputs sl-button[name="run"]').click()
+
+    cy.get('#lsmReadImage-could-read-details').should('contain', 'true')
+    cy.get('#lsmReadImage-image-details').should('contain', 'imageType')
+  })
+})
