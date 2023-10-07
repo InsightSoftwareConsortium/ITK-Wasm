db3740afac6fa8679e2bf1080dab5cd929809645
Matt McCormick <matt.mccormick@kitware.com>
2023-10-07 19:38:28 -0400

    test(image-io): port TIFF test to package

---
+++ packages/image-io/typescript/cypress/e2e/tiff.cy.ts
@@ -0,0 +1,44 @@
+import { demoServer } from './common.ts'
+
+describe('tiff', () => {
+  beforeEach(function() {
+    cy.visit(demoServer)
+
+    const testPathPrefix = '../test/data/input/'
+
+    const testImageFiles = [
+      'ShortTestImage.tiff',
+      'ShortTestImage.iwi.cbor',
+    ]
+    testImageFiles.forEach((fileName) => {
+      cy.readFile(`${testPathPrefix}${fileName}`, null).as(fileName)
+    })
+  })
+
+  it('Reads a TIFF image', function () {
+    cy.get('sl-tab[panel="tiffReadImage-panel"]').click()
+
+    const testFile = { contents: new Uint8Array(this['ShortTestImage.tiff']), fileName: 'ShortTestImage.tiff' }
+    cy.get('#tiffReadImageInputs input[name="serialized-image-file"]').selectFile([testFile,], { force: true })
+    cy.get('#tiffReadImage-serialized-image-details').should('contain', '73,73')
+
+    cy.get('#tiffReadImageInputs sl-button[name="run"]').click()
+
+    cy.get('#tiffReadImage-could-read-details').should('contain', 'true')
+    cy.get('#tiffReadImage-image-details').should('contain', 'imageType')
+  })
+
+  it('Writes a TIFF image', function () {
+    cy.get('sl-tab[panel="tiffWriteImage-panel"]').click()
+
+    const testFile = { contents: new Uint8Array(this['ShortTestImage.iwi.cbor']), fileName: 'ShortTestImage.iwi.cbor' }
+    cy.get('#tiffWriteImageInputs input[name="image-file"]').selectFile([testFile,], { force: true })
+    cy.get('#tiffWriteImage-image-details').should('contain', 'imageType')
+    cy.get('#tiffWriteImageInputs sl-input[name="serialized-image"]').find('input', { includeShadowDom: true }).type('ShortTestImage.tiff', { force: true })
+
+    cy.get('#tiffWriteImageInputs sl-button[name="run"]').click()
+
+    cy.get('#tiffWriteImage-could-write-details').should('contain', 'true')
+    cy.get('#tiffWriteImage-serialized-image-details').should('contain', '73,73')
+  })
+})
---
+++ packages/image-io/typescript/test/node/tiff-test.js
@@ -0,0 +1,47 @@
+import test from 'ava'
+import path from 'path'
+
+import { tiffReadImageNode, tiffWriteImageNode } from '../../dist/bundles/image-io-node.js'
+import { IntTypes, PixelTypes, getMatrixElement } from 'itk-wasm'
+
+import { testInputPath, testOutputPath } from './common.js'
+
+const testInputFilePath = path.join(testInputPath, 'ShortTestImage.tiff')
+const testOutputFilePath = path.join(testOutputPath, 'tiff-test-ShortTestImage.tiff')
+
+const verifyImage = (t, image) => {
+  t.is(image.imageType.dimension, 2, 'dimension')
+  t.is(image.imageType.componentType, IntTypes.Int16, 'componentType')
+  t.is(image.imageType.pixelType, PixelTypes.Scalar, 'pixelType')
+  t.is(image.imageType.components, 1, 'components')
+  t.is(image.origin[0], 0.0, 'origin[0]')
+  t.is(image.origin[1], 0.0, 'origin[1]')
+  // t.is(image.spacing[0], 1.0, 'spacing[0]')
+  // t.is(image.spacing[1], 1.0, 'spacing[1]')
+  t.is(getMatrixElement(image.direction, 2, 0, 0), 1.0, 'direction (0, 0)')
+  t.is(getMatrixElement(image.direction, 2, 0, 1), 0.0, 'direction (0, 1)')
+  t.is(getMatrixElement(image.direction, 2, 1, 0), 0.0, 'direction (1, 0)')
+  t.is(getMatrixElement(image.direction, 2, 1, 1), 1.0, 'direction (1, 1)')
+  t.is(image.size[0], 20, 'size[0]')
+  t.is(image.size[1], 20, 'size[1]')
+  t.is(image.data.length, 400, 'data.length')
+  t.is(image.data[100], -9, 'data[100]')
+}
+
+test('Test reading a TIFF file', async t => {
+  const { couldRead, image } = await tiffReadImageNode(testInputFilePath)
+  t.true(couldRead)
+  verifyImage(t, image)
+})
+
+test('Test writing a TIFF file', async t => {
+  const { couldRead, image } = await tiffReadImageNode(testInputFilePath)
+  t.true(couldRead)
+  const useCompression = false
+  const { couldWrite } = await tiffWriteImageNode(image, testOutputFilePath, { useCompression })
+  t.true(couldWrite)
+
+  const { couldRead: couldReadBack, image: imageBack } = await tiffReadImageNode(testOutputFilePath)
+  t.true(couldReadBack)
+  verifyImage(t, imageBack)
+})
