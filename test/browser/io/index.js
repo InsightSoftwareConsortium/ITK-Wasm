78218d4f30261aa56a3b99e86e702a5289af7049
Matt McCormick <matt.mccormick@kitware.com>
2023-10-10 16:16:17 -0400

    test(io): remove tests ported to the image-io package

--- test/browser/io/index.js
+++ test/browser/io/index.js
@@ -1,20 +1,16 @@
-import readImageTest from './readImageTest.js'
 import readImageFileSeriesTest from './readImageFileSeriesTest.js'
 
 import readMeshTest from './readMeshTest.js'
 
-import writeImageTest from './writeImageTest.js'
 import writeMeshTest from './writeMeshTest.js'
 
 import meshToPolyDataTest from './meshToPolyDataTest.js'
 
 export default function () {
-  readImageTest()
   readImageFileSeriesTest()
 
   readMeshTest()
 
-  writeImageTest()
   writeMeshTest()
 
   meshToPolyDataTest()
