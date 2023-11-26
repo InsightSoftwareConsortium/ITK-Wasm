dbeb159a3e46afe14c51fe8d01f2469430f931ad
Matt McCormick <matt.mccormick@kitware.com>
2023-11-26 13:33:16 -0500

    test(mesh-io): add byu-test

---
+++ packages/mesh-io/typescript/test/node/byu-test.js
@@ -0,0 +1,39 @@
+import test from 'ava'
+import path from 'path'
+
+import { byuReadMeshNode, byuWriteMeshNode } from '../../dist/index-node.js'
+import { IntTypes, FloatTypes, PixelTypes } from 'itk-wasm'
+
+import { testInputPath, testOutputPath } from './common.js'
+
+const testInputFilePath = path.resolve(testInputPath, 'cube.byu')
+const testOutputFilePath = path.resolve(testOutputPath, 'byu-test-cube.byu')
+
+const verifyMesh = (t, mesh) => {
+  t.is(mesh.meshType.dimension, 3)
+  t.is(mesh.meshType.pointComponentType, FloatTypes.Float64)
+  t.is(mesh.meshType.cellComponentType, IntTypes.UInt32)
+  t.is(mesh.meshType.pointPixelType, PixelTypes.Scalar)
+  t.is(mesh.meshType.cellPixelType, PixelTypes.Scalar)
+  t.is(mesh.numberOfPoints, 8)
+  t.is(mesh.numberOfCells, 6)
+}
+
+test('readMeshLocalFile reads a BYU file path given on the local filesystem', async (t) => {
+  const { couldRead, mesh } = await byuReadMeshNode(testInputFilePath)
+  t.true(couldRead)
+  verifyMesh(t, mesh)
+})
+
+test('writeMeshLocalFile writes a BYU file path on the local filesystem', async (t) => {
+  const { couldRead, mesh } = await byuReadMeshNode(testInputFilePath)
+  t.true(couldRead)
+
+  const useCompression = false
+  const { couldWrite } = await byuWriteMeshNode(mesh, testOutputFilePath, { useCompression })
+  t.true(couldWrite)
+
+  const { couldRead: couldReadBack, mesh: meshBack } = await byuReadMeshNode(testOutputFilePath)
+  t.true(couldReadBack)
+  verifyMesh(t, meshBack)
+})
