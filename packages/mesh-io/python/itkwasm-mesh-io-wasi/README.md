9fce9fbd014dbc6b6d7c5aed349b12b2770466d5
Matt McCormick <matt.mccormick@kitware.com>
2023-11-28 13:36:33 -0500

    feat(mesh-io): bindgen wasi output

---
+++ packages/mesh-io/python/itkwasm-mesh-io-wasi/README.md
@@ -0,0 +1,26 @@
+# itkwasm-mesh-io-wasi
+
+[![PyPI version](https://badge.fury.io/py/itkwasm-mesh-io-wasi.svg)](https://badge.fury.io/py/itkwasm-mesh-io-wasi)
+
+Input and output for scientific and medical image file formats. WASI implementation.
+
+This package provides the WASI WebAssembly implementation. It is usually not called directly. Please use [`itkwasm-mesh-io`](https://pypi.org/project/itkwasm-mesh-io/) instead.
+
+
+## Installation
+
+```sh
+pip install itkwasm-mesh-io-wasi
+```
+
+## Development
+
+```sh
+pip install pytest
+pip install -e .
+pytest
+
+# or
+pip install hatch
+hatch run test
+```
---
+++ packages/mesh-io/python/itkwasm-mesh-io-wasi/itkwasm_mesh_io_wasi/__init__.py
@@ -0,0 +1,26 @@
+# Generated file. To retain edits, remove this comment.
+
+"""itkwasm-mesh-io-wasi: Input and output for scientific and medical image file formats. WASI implementation."""
+
+from .byu_read_mesh import byu_read_mesh
+from .byu_write_mesh import byu_write_mesh
+from .free_surfer_ascii_read_mesh import free_surfer_ascii_read_mesh
+from .free_surfer_ascii_write_mesh import free_surfer_ascii_write_mesh
+from .free_surfer_binary_read_mesh import free_surfer_binary_read_mesh
+from .free_surfer_binary_write_mesh import free_surfer_binary_write_mesh
+from .obj_read_mesh import obj_read_mesh
+from .obj_write_mesh import obj_write_mesh
+from .off_read_mesh import off_read_mesh
+from .off_write_mesh import off_write_mesh
+from .stl_read_mesh import stl_read_mesh
+from .stl_write_mesh import stl_write_mesh
+from .swc_read_mesh import swc_read_mesh
+from .swc_write_mesh import swc_write_mesh
+from .vtk_poly_data_read_mesh import vtk_poly_data_read_mesh
+from .vtk_poly_data_write_mesh import vtk_poly_data_write_mesh
+from .wasm_read_mesh import wasm_read_mesh
+from .wasm_write_mesh import wasm_write_mesh
+from .wasm_zstd_read_mesh import wasm_zstd_read_mesh
+from .wasm_zstd_write_mesh import wasm_zstd_write_mesh
+
+from ._version import __version__
