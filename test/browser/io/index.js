d95da7ab0ebc36d246860df56dd2cbc31352e517
Matt McCormick <matt.mccormick@kitware.com>
2023-11-28 09:42:57 -0500

    test: remove migrated read and write mesh tests
    Now in mesh-io package.

--- test/browser/io/index.js
+++ test/browser/io/index.js
@@ -1,17 +1,9 @@
 import readImageFileSeriesTest from './readImageFileSeriesTest.js'
 
-import readMeshTest from './readMeshTest.js'
-
-import writeMeshTest from './writeMeshTest.js'
-
 import meshToPolyDataTest from './meshToPolyDataTest.js'
 
 export default function () {
   readImageFileSeriesTest()
-
-  readMeshTest()
-
-  writeMeshTest()
 
   meshToPolyDataTest()
 }
