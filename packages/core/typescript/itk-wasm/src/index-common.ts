7a357c46bc19ec0ea065cae4e7a720c29922dad3
Matt McCormick <matt.mccormick@kitware.com>
2023-12-01 20:00:51 -0500

    test(core): add node:test float-types-test

--- packages/core/typescript/itk-wasm/src/index-common.ts
+++ packages/core/typescript/itk-wasm/src/index-common.ts
@@ -1,3 +1,5 @@
 // Core API interfaces, data structures, and functions
 
 export { default as bufferToTypedArray } from './buffer-to-typed-array.js'
+
+export * from './interface-types/index.js'
---
+++ packages/core/typescript/itk-wasm/src/interface-types/index.ts
@@ -0,0 +1,4 @@
+// itk-wasm interface types
+
+import FloatTypes from './float-types.js'
+export { FloatTypes }
