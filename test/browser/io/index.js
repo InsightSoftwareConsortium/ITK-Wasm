a02d7096bbeea7bc2674ef3f90fd54c901a88ff5
Matt McCormick <matt.mccormick@kitware.com>
2023-12-14 23:11:48 -0500

    refactor: remove tests migrated to packages or not relevant

--- test/browser/io/index.js
+++
@@ -1,9 +0,0 @@
-import readImageFileSeriesTest from './readImageFileSeriesTest.js'
-
-import meshToPolyDataTest from './meshToPolyDataTest.js'
-
-export default function () {
-  readImageFileSeriesTest()
-
-  meshToPolyDataTest()
-}
