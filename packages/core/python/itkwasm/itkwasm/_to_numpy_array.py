0c41e46c6241c50c6eaeff2a3b9518dde29f82e3
Matt McCormick <matt.mccormick@kitware.com>
2024-01-24 21:09:05 -0500

    feat(python): add image_from_array to itkwasm

--- packages/core/python/itkwasm/itkwasm/_to_numpy_array.py
+++ packages/core/python/itkwasm/itkwasm/_to_numpy_array.py
@@ -26,3 +26,27 @@ def _to_numpy_array(component_type, buf):
         return np.frombuffer(buf, dtype=np.float64)
     else:
         raise ValueError('Unsupported component type')
+
+def _dtype_to_component_type(dtype):
+    if dtype == np.uint8:
+        return IntTypes.UInt8
+    elif dtype == np.int8:
+        return IntTypes.Int8
+    elif dtype == np.uint16:
+        return IntTypes.UInt16
+    elif dtype == np.int16:
+        return IntTypes.Int16
+    elif dtype == np.uint32:
+        return IntTypes.UInt32
+    elif dtype == np.int32:
+        return IntTypes.Int32
+    elif dtype == np.uint64:
+        return IntTypes.UInt64
+    elif dtype == np.int64:
+        return IntTypes.Int64
+    elif dtype == np.float32:
+        return FloatTypes.Float32
+    elif dtype == np.float64:
+        return FloatTypes.Float64
+    else:
+        raise ValueError('Unsupported dtype')
---
+++ packages/core/python/itkwasm/itkwasm/image_from_array.py
@@ -0,0 +1,44 @@
+from typing import Optional
+try:
+    from numpy.typing import ArrayLike
+except ImportError:
+    from numpy import ndarray as ArrayLike
+
+from .image import Image, ImageType
+from ._to_numpy_array import _dtype_to_component_type
+
+from .pixel_types import PixelTypes
+
+def image_from_array(arr, is_vector: bool = False, image_type: Optional[ImageType] = None) -> Image:
+    """Convert a numpy array-like to an itkwasm Image.
+
+    :param arr: Input n-d array
+    :type  arr: NumPy ndarray or array-like, e.g. dask.Array or cupy.ndarray
+
+    :param is_vector: Whether the array is a vector image. If True, then a 3D array will be treated as a 2D vector image, otherwise it will be treated as a 3D image.
+    :type  is_vector: bool
+
+    :param image_type: Explicitly specify the Image type
+    :type  image_type: ImageType
+
+    :return: Output Image
+    :rtype : Image where pixel data is a view of the input array and default metadata
+    """
+
+    dimension = arr.ndim
+    if is_vector:
+        dimension -= 1
+
+    if image_type is None:
+        image_type = ImageType(
+            dimension=dimension,
+            componentType=_dtype_to_component_type(arr.dtype),
+            pixelType=PixelTypes.Scalar if not is_vector else PixelTypes.VariableLengthVector,
+            components=arr.shape[-1] if is_vector else 1,
+        )
+
+    image = Image(imageType=image_type)
+    image.size = arr.shape[:dimension]
+    image.data = arr
+
+    return image
