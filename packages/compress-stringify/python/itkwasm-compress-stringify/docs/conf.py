69de834df9011b8aa4b71eaf24b999140341750f
Matt McCormick <matt.mccormick@kitware.com>
2024-01-29 21:13:13 -0500

    fix(python): update package html theme source_directory

--- packages/compress-stringify/python/itkwasm-compress-stringify/docs/conf.py
+++ packages/compress-stringify/python/itkwasm-compress-stringify/docs/conf.py
@@ -53,5 +53,5 @@ html_theme_options = {
     "top_of_page_button": "edit",
     "source_repository": "https://github.com/InsightSoftwareConsortium/itk-wasm",
     "source_branch": "main",
-    "source_directory": "docs",
+    "source_directory": "packages/compress-stringify/python/itkwasm-compress-stringify/docs",
 }
