75da4d816253e2eaa07012050cbf3178e2d00ca4
Paul Elliott <paul.elliott@kitware.com>
2024-03-18 14:49:33 -0400

    feat(image-sets-normalization): Sort DICOMs
    Use GDCM discriminate volume example class.

--- packages/dicom/gdcm/image-sets-normalization.cxx
+++ packages/dicom/gdcm/image-sets-normalization.cxx
@@ -23,6 +23,8 @@
 #include "rapidjson/stringbuffer.h"
 #include "rapidjson/writer.h"
 
+#include "discriminate-volume.cxx"
+
 int main(int argc, char *argv[])
 {
   itk::wasm::Pipeline pipeline("image-sets-normalization", "Group DICOM files into image sets", argc, argv);
@@ -35,13 +37,57 @@ int main(int argc, char *argv[])
 
   ITK_WASM_PARSE(pipeline);
 
-  rapidjson::Document imageSetsJson;
-  imageSetsJson.SetObject();
-  rapidjson::Document::AllocatorType &allocator = imageSetsJson.GetAllocator();
+  gdcm::Scanner s;
 
-  rapidjson::Value almostEqualValue;
-  almostEqualValue.SetBool(false);
-  imageSetsJson.AddMember("", almostEqualValue, allocator);
+  const gdcm::Tag t1(0x0020, 0x000d); // Study Instance UID
+  const gdcm::Tag t2(0x0020, 0x000e); // Series Instance UID
+  const gdcm::Tag t3(0x0020, 0x0052); // Frame of Reference UID
+  const gdcm::Tag t4(0x0020, 0x0037); // Image Orientation (Patient)
+
+  s.AddTag(t1);
+  s.AddTag(t2);
+  s.AddTag(t3);
+  s.AddTag(t4);
+
+  bool b = s.Scan(files);
+  if (!b)
+  {
+    std::cerr << "Scanner failed" << std::endl;
+    return 1;
+  }
+
+  gdcm::DiscriminateVolume dv;
+  dv.ProcessIntoVolume(s);
+
+  std::vector<gdcm::Directory::FilenamesType> sortedFiles = dv.GetSortedFiles();
+
+  rapidjson::Document imageSetsJson;
+  rapidjson::Document::AllocatorType &allocator = imageSetsJson.GetAllocator();
+  imageSetsJson.SetObject();
+
+  int groupId = 0;
+  for (
+      std::vector<gdcm::Directory::FilenamesType>::const_iterator fileNames = sortedFiles.begin();
+      fileNames != sortedFiles.end(); ++fileNames)
+  {
+    groupId++;
+    rapidjson::Value sortedFileNameArray(rapidjson::kArrayType);
+
+    for (
+        gdcm::Directory::FilenamesType::const_iterator fileName = fileNames->begin();
+        fileName != fileNames->end(); ++fileName)
+    {
+      rapidjson::Value fileNameValue;
+      fileNameValue.SetString(fileName->c_str(), fileName->size(), allocator);
+      sortedFileNameArray.PushBack(fileNameValue, allocator);
+    }
+
+    std::string fileName = fileNames->front();
+    const char* studyInstanceUID = s.GetValue(fileName.c_str(), t1);
+
+    rapidjson::Value groupIdKey(studyInstanceUID, allocator);
+    imageSetsJson.AddMember(groupIdKey, sortedFileNameArray, allocator);
+  }
 
   rapidjson::StringBuffer stringBuffer;
   rapidjson::Writer<rapidjson::StringBuffer> writer(stringBuffer);
--- packages/dicom/python/itkwasm-dicom-wasi/tests/test_image_sets_normalization.py
+++ packages/dicom/python/itkwasm-dicom-wasi/tests/test_image_sets_normalization.py
@@ -3,14 +3,35 @@ from itkwasm_dicom_wasi import image_sets_normalization
 from .common import test_input_path, test_output_path
 
 
-def test_image_sets_normalization():
+def test_one_series():
     files = [
+        test_input_path / "DicomImageOrientationTest" / "ImageOrientation.3.dcm",
         test_input_path / "DicomImageOrientationTest" / "ImageOrientation.1.dcm",
         test_input_path / "DicomImageOrientationTest" / "ImageOrientation.2.dcm",
-        test_input_path / "DicomImageOrientationTest" / "ImageOrientation.3.dcm",
     ]
 
     assert files[0].exists()
 
-    output_text = image_sets_normalization(files)
-    assert output_text
+    image_sets = image_sets_normalization(files)
+    print(image_sets)
+    # assert image_sets
+
+    assert image_sets == [
+        str(files[1]),
+        str(files[2]),
+        str(files[0]),
+    ]
+
+# def test_two_series():
+#     files = [
+#         test_input_path / "DicomImageOrientationTest" / "ImageOrientation.3.dcm",
+#         test_input_path / "DicomImageOrientationTest" / "ImageOrientation.1.dcm",
+#         test_input_path / "DicomImageOrientationTest" / "ImageOrientation.2.dcm",
+#         test_input_path / "dicom-images" / "CT" / "1-1.dcm",
+#         test_input_path / "dicom-images" / "CT" / "1-2.dcm",
+#     ]
+
+#     assert files[0].exists()
+
+#     image_sets = image_sets_normalization(files)
+#     assert image_sets
