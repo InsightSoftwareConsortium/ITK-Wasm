d1c116f8e464b1e0c640594de7f27908ca9c6c16
Matt McCormick <matt.mccormick@kitware.com>
2024-03-20 21:58:08 -0400

    refactor(compress-stringify): use application/zstd as default mime-type
    https://datatracker.ietf.org/doc/html/rfc8878#name-the-application-zstd-media-

--- packages/compress-stringify/typescript/test/node/compress-stringify-test.js
+++ packages/compress-stringify/typescript/test/node/compress-stringify-test.js
@@ -1,40 +1,62 @@
-import test from 'ava'
+import test from "ava";
 
-import { compressStringifyNode, parseStringDecompressNode } from '../../dist/index-node.js'
+import {
+  compressStringifyNode,
+  parseStringDecompressNode,
+} from "../../dist/index-node.js";
 
-test('Decompress returns what was compressed', async t => {
-  const data = new Uint8Array([222, 173, 190, 239])
-  const { output: compressedData } = await compressStringifyNode(data, { compressionLevel: 8 })
-  const { output: decompressedData } = await parseStringDecompressNode(compressedData)
+test("Decompress returns what was compressed", async (t) => {
+  const data = new Uint8Array([222, 173, 190, 239]);
+  const { output: compressedData } = await compressStringifyNode(data, {
+    compressionLevel: 8,
+  });
+  const { output: decompressedData } =
+    await parseStringDecompressNode(compressedData);
 
-  t.is(decompressedData[0], 222)
-  t.is(decompressedData[1], 173)
-  t.is(decompressedData[2], 190)
-  t.is(decompressedData[3], 239)
-})
+  t.is(decompressedData[0], 222);
+  t.is(decompressedData[1], 173);
+  t.is(decompressedData[2], 190);
+  t.is(decompressedData[3], 239);
+});
 
-test('We can stringify during compression', async t => {
-  const data = new Uint8Array([222, 173, 190, 239])
-  const { output: compressedData } = await compressStringifyNode(data, { compressionLevel: 8, stringify: true })
-  const decoder = new TextDecoder()
-  t.is(decoder.decode(compressedData.buffer), 'data:base64,KLUv/SAEIQAA3q2+7w==')
-  const { output: decompressedData } = await parseStringDecompressNode(compressedData, { parseString: true })
+test("We can stringify during compression", async (t) => {
+  const data = new Uint8Array([222, 173, 190, 239]);
+  const { output: compressedData } = await compressStringifyNode(data, {
+    compressionLevel: 8,
+    stringify: true,
+  });
+  const decoder = new TextDecoder();
+  t.is(
+    decoder.decode(compressedData.buffer),
+    "data:application/zstd;base64,KLUv/SAEIQAA3q2+7w==",
+  );
+  const { output: decompressedData } = await parseStringDecompressNode(
+    compressedData,
+    { parseString: true },
+  );
 
-  t.is(decompressedData[0], 222)
-  t.is(decompressedData[1], 173)
-  t.is(decompressedData[2], 190)
-  t.is(decompressedData[3], 239)
-})
+  t.is(decompressedData[0], 222);
+  t.is(decompressedData[1], 173);
+  t.is(decompressedData[2], 190);
+  t.is(decompressedData[3], 239);
+});
 
-test('We can use a custom dataUrlPrefix', async t => {
-  const data = new Uint8Array([222, 173, 190, 239])
-  const { output: compressedData } = await compressStringifyNode(data, { compressionLevel: 8, stringify: true, dataUrlPrefix: 'custom,' })
-  const decoder = new TextDecoder()
-  t.is(decoder.decode(compressedData.buffer), 'custom,KLUv/SAEIQAA3q2+7w==')
-  const { output: decompressedData } = await parseStringDecompressNode(compressedData, { parseString: true })
+test("We can use a custom dataUrlPrefix", async (t) => {
+  const data = new Uint8Array([222, 173, 190, 239]);
+  const { output: compressedData } = await compressStringifyNode(data, {
+    compressionLevel: 8,
+    stringify: true,
+    dataUrlPrefix: "custom,",
+  });
+  const decoder = new TextDecoder();
+  t.is(decoder.decode(compressedData.buffer), "custom,KLUv/SAEIQAA3q2+7w==");
+  const { output: decompressedData } = await parseStringDecompressNode(
+    compressedData,
+    { parseString: true },
+  );
 
-  t.is(decompressedData[0], 222)
-  t.is(decompressedData[1], 173)
-  t.is(decompressedData[2], 190)
-  t.is(decompressedData[3], 239)
-})
+  t.is(decompressedData[0], 222);
+  t.is(decompressedData[1], 173);
+  t.is(decompressedData[2], 190);
+  t.is(decompressedData[3], 239);
+});
