f406caf387afc2c09921acbdc3dda03fe9ce26ff
Matt McCormick <matt.mccormick@kitware.com>
2024-04-03 10:51:21 -0400

    style(compress-stringify): black format test_interface_type_json

--- packages/compress-stringify/python/itkwasm-compress-stringify/test/test_interface_type_json.py
+++ packages/compress-stringify/python/itkwasm-compress-stringify/test/test_interface_type_json.py
@@ -1,15 +1,35 @@
 from pathlib import Path
 
-from itkwasm_compress_stringify import compress_stringify, parse_string_decompress, image_to_json, json_to_image
+from itkwasm_compress_stringify import (
+    compress_stringify,
+    parse_string_decompress,
+    image_to_json,
+    json_to_image,
+)
+
 
 def test_image_to_json():
     from itkwasm_image_io import read_image
     from itkwasm_compare_images import compare_images
 
-    image_filepath = Path(__file__).parent / ".." / ".." / ".." / "test" / "data" / "input" / "cthead1.png"
+    image_filepath = (
+        Path(__file__).parent
+        / ".."
+        / ".."
+        / ".."
+        / "test"
+        / "data"
+        / "input"
+        / "cthead1.png"
+    )
     image = read_image(image_filepath)
 
     image_json = image_to_json(image)
     json_image = json_to_image(image_json)
-    almost_equal, _, _ = compare_images(image, [json_image,])
+    almost_equal, _, _ = compare_images(
+        image,
+        [
+            json_image,
+        ],
+    )
     assert almost_equal
