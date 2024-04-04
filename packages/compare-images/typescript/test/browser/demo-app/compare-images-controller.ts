05ffb8f0db526f0c14c1afb2cc7ddeca0cc5aa78
Paul Elliott <paul.elliott@kitware.com>
2024-04-04 15:38:28 -0400

    chore(compare-images): update demo app to use itk-image-details

--- packages/compare-images/typescript/test/browser/demo-app/compare-images-controller.ts
+++ packages/compare-images/typescript/test/browser/demo-app/compare-images-controller.ts
@@ -49,8 +49,7 @@ class CompareImagesController  {
         webWorker.terminate()
         model.inputs.set("testImage", image)
         const details = document.getElementById("compareImages-test-image-details")
-        details.innerHTML = `<pre>${globalThis.escapeHtml(JSON.stringify(image, globalThis.interfaceTypeJsonReplacer, 2))}</pre>`
-        details.disabled = false
+        details.setImage(image)
     })
 
     // ----------------------------------------------
@@ -168,17 +167,13 @@ class CompareImagesController  {
         differenceImageOutputDownload.variant = "success"
         differenceImageOutputDownload.disabled = false
         const differenceImageDetails = document.getElementById("compareImages-difference-image-details")
-        differenceImageDetails.innerHTML = `<pre>${globalThis.escapeHtml(JSON.stringify(differenceImage, globalThis.interfaceTypeJsonReplacer, 2))}</pre>`
-        differenceImageDetails.disabled = false
-        const differenceImageOutput = document.getElementById('compareImages-difference-image-details')
+        differenceImageDetails.setImage(differenceImage)
 
         model.outputs.set("differenceUchar2dImage", differenceUchar2dImage)
         differenceUchar2dImageOutputDownload.variant = "success"
         differenceUchar2dImageOutputDownload.disabled = false
         const differenceUchar2dImageDetails = document.getElementById("compareImages-difference-uchar-2d-image-details")
-        differenceUchar2dImageDetails.innerHTML = `<pre>${globalThis.escapeHtml(JSON.stringify(differenceUchar2dImage, globalThis.interfaceTypeJsonReplacer, 2))}</pre>`
-        differenceUchar2dImageDetails.disabled = false
-        const differenceUchar2dImageOutput = document.getElementById('compareImages-difference-uchar-2d-image-details')
+        differenceUchar2dImageDetails.setImage(differenceUchar2dImage)
       } catch (error) {
         globalThis.notify("Error while running pipeline", error.toString(), "danger", "exclamation-octagon")
         throw error
