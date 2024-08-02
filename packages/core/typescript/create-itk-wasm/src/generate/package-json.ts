d062945c4e8ab2e7b8589163d40af543e58f32cb
Matt McCormick <matt.mccormick@kitware.com>
2024-08-02 10:46:56 -0400

    chore: bump pnpm to 9.6.0

--- packages/core/typescript/create-itk-wasm/src/generate/package-json.ts
+++ packages/core/typescript/create-itk-wasm/src/generate/package-json.ts
@@ -21,7 +21,7 @@ function generatePackageJson(project: ProjectSpec) {
     // @ts-ignore
     itkWasm.repository = project.repositoryUrl
   }
-  const packageManager = 'pnpm@9.4.0'
+  const packageManager = 'pnpm@9.6.0'
   let packageJson = {
     name: `${project.name}-build`,
     version: '0.1.0',
