27191947f4f56ac387d73a430e4650b8d93893cb
Matt McCormick <matt.mccormick@kitware.com>
2024-08-10 21:31:29 -0400

    build(index-worker-embedded): specify pipelineWorker type
    Avoid implicit `any`. `string` may not be technically right -- this
    `.worker.js` is a worker module source treated specially by bundlers.

--- packages/core/typescript/itk-wasm/src/bindgen/typescript/resources/index-worker-embedded.ts
+++ packages/core/typescript/itk-wasm/src/bindgen/typescript/resources/index-worker-embedded.ts
@@ -2,6 +2,6 @@
 
 import { setPipelineWorkerUrl } from './index.js'
 import pipelineWorker from '../node_modules/itk-wasm/dist/pipeline/web-workers/bundles/itk-wasm-pipeline.worker.js'
-setPipelineWorkerUrl(pipelineWorker)
+setPipelineWorkerUrl(pipelineWorker as string)
 
 export * from './index.js'
