ded6ed8bb543ffec0a52dbe955212db309a1d87b
Matt McCormick <matt@mmmccormick.com>
2024-11-08 07:24:32 -0500

    feat(transform-io): add read-transform-node

--- packages/transform-io/typescript/src/index-common.ts
+++ packages/transform-io/typescript/src/index-common.ts
@@ -1,6 +1,6 @@
 // Generated file. To retain edits, remove this comment.
 
-export { default as version } from './version.js'
+export { default as version } from "./version.js";
 
-export type { JsonCompatible } from 'itk-wasm'
-export type { TransformList } from 'itk-wasm'
+export type { JsonCompatible } from "itk-wasm";
+export type { TransformList } from "itk-wasm";
--- packages/transform-io/typescript/src/index-node-only.ts
+++ packages/transform-io/typescript/src/index-node-only.ts
@@ -1,122 +1,113 @@
-// Generated file. To retain edits, remove this comment.
+import ReadTransformOptions from "./read-transform-options.js";
+export type { ReadTransformOptions };
 
+import readTransformNode from "./read-transform-node.js";
+export { readTransformNode };
 
+import Hdf5ReadTransformNodeResult from "./hdf5-read-transform-node-result.js";
+export type { Hdf5ReadTransformNodeResult };
 
-import Hdf5ReadTransformNodeResult from './hdf5-read-transform-node-result.js'
-export type { Hdf5ReadTransformNodeResult }
+import Hdf5ReadTransformNodeOptions from "./hdf5-read-transform-node-options.js";
+export type { Hdf5ReadTransformNodeOptions };
 
-import Hdf5ReadTransformNodeOptions from './hdf5-read-transform-node-options.js'
-export type { Hdf5ReadTransformNodeOptions }
+import hdf5ReadTransformNode from "./hdf5-read-transform-node.js";
+export { hdf5ReadTransformNode };
 
-import hdf5ReadTransformNode from './hdf5-read-transform-node.js'
-export { hdf5ReadTransformNode }
+import Hdf5WriteTransformNodeResult from "./hdf5-write-transform-node-result.js";
+export type { Hdf5WriteTransformNodeResult };
 
+import Hdf5WriteTransformNodeOptions from "./hdf5-write-transform-node-options.js";
+export type { Hdf5WriteTransformNodeOptions };
 
-import Hdf5WriteTransformNodeResult from './hdf5-write-transform-node-result.js'
-export type { Hdf5WriteTransformNodeResult }
+import hdf5WriteTransformNode from "./hdf5-write-transform-node.js";
+export { hdf5WriteTransformNode };
 
-import Hdf5WriteTransformNodeOptions from './hdf5-write-transform-node-options.js'
-export type { Hdf5WriteTransformNodeOptions }
+import MatReadTransformNodeResult from "./mat-read-transform-node-result.js";
+export type { MatReadTransformNodeResult };
 
-import hdf5WriteTransformNode from './hdf5-write-transform-node.js'
-export { hdf5WriteTransformNode }
+import MatReadTransformNodeOptions from "./mat-read-transform-node-options.js";
+export type { MatReadTransformNodeOptions };
 
+import matReadTransformNode from "./mat-read-transform-node.js";
+export { matReadTransformNode };
 
-import MatReadTransformNodeResult from './mat-read-transform-node-result.js'
-export type { MatReadTransformNodeResult }
+import MatWriteTransformNodeResult from "./mat-write-transform-node-result.js";
+export type { MatWriteTransformNodeResult };
 
-import MatReadTransformNodeOptions from './mat-read-transform-node-options.js'
-export type { MatReadTransformNodeOptions }
+import MatWriteTransformNodeOptions from "./mat-write-transform-node-options.js";
+export type { MatWriteTransformNodeOptions };
 
-import matReadTransformNode from './mat-read-transform-node.js'
-export { matReadTransformNode }
+import matWriteTransformNode from "./mat-write-transform-node.js";
+export { matWriteTransformNode };
 
+import MncReadTransformNodeResult from "./mnc-read-transform-node-result.js";
+export type { MncReadTransformNodeResult };
 
-import MatWriteTransformNodeResult from './mat-write-transform-node-result.js'
-export type { MatWriteTransformNodeResult }
+import MncReadTransformNodeOptions from "./mnc-read-transform-node-options.js";
+export type { MncReadTransformNodeOptions };
 
-import MatWriteTransformNodeOptions from './mat-write-transform-node-options.js'
-export type { MatWriteTransformNodeOptions }
+import mncReadTransformNode from "./mnc-read-transform-node.js";
+export { mncReadTransformNode };
 
-import matWriteTransformNode from './mat-write-transform-node.js'
-export { matWriteTransformNode }
+import MncWriteTransformNodeResult from "./mnc-write-transform-node-result.js";
+export type { MncWriteTransformNodeResult };
 
+import MncWriteTransformNodeOptions from "./mnc-write-transform-node-options.js";
+export type { MncWriteTransformNodeOptions };
 
-import MncReadTransformNodeResult from './mnc-read-transform-node-result.js'
-export type { MncReadTransformNodeResult }
+import mncWriteTransformNode from "./mnc-write-transform-node.js";
+export { mncWriteTransformNode };
 
-import MncReadTransformNodeOptions from './mnc-read-transform-node-options.js'
-export type { MncReadTransformNodeOptions }
+import TxtReadTransformNodeResult from "./txt-read-transform-node-result.js";
+export type { TxtReadTransformNodeResult };
 
-import mncReadTransformNode from './mnc-read-transform-node.js'
-export { mncReadTransformNode }
+import TxtReadTransformNodeOptions from "./txt-read-transform-node-options.js";
+export type { TxtReadTransformNodeOptions };
 
+import txtReadTransformNode from "./txt-read-transform-node.js";
+export { txtReadTransformNode };
 
-import MncWriteTransformNodeResult from './mnc-write-transform-node-result.js'
-export type { MncWriteTransformNodeResult }
+import TxtWriteTransformNodeResult from "./txt-write-transform-node-result.js";
+export type { TxtWriteTransformNodeResult };
 
-import MncWriteTransformNodeOptions from './mnc-write-transform-node-options.js'
-export type { MncWriteTransformNodeOptions }
+import TxtWriteTransformNodeOptions from "./txt-write-transform-node-options.js";
+export type { TxtWriteTransformNodeOptions };
 
-import mncWriteTransformNode from './mnc-write-transform-node.js'
-export { mncWriteTransformNode }
+import txtWriteTransformNode from "./txt-write-transform-node.js";
+export { txtWriteTransformNode };
 
+import WasmReadTransformNodeResult from "./wasm-read-transform-node-result.js";
+export type { WasmReadTransformNodeResult };
 
-import TxtReadTransformNodeResult from './txt-read-transform-node-result.js'
-export type { TxtReadTransformNodeResult }
+import WasmReadTransformNodeOptions from "./wasm-read-transform-node-options.js";
+export type { WasmReadTransformNodeOptions };
 
-import TxtReadTransformNodeOptions from './txt-read-transform-node-options.js'
-export type { TxtReadTransformNodeOptions }
+import wasmReadTransformNode from "./wasm-read-transform-node.js";
+export { wasmReadTransformNode };
 
-import txtReadTransformNode from './txt-read-transform-node.js'
-export { txtReadTransformNode }
+import WasmWriteTransformNodeResult from "./wasm-write-transform-node-result.js";
+export type { WasmWriteTransformNodeResult };
 
+import WasmWriteTransformNodeOptions from "./wasm-write-transform-node-options.js";
+export type { WasmWriteTransformNodeOptions };
 
-import TxtWriteTransformNodeResult from './txt-write-transform-node-result.js'
-export type { TxtWriteTransformNodeResult }
+import wasmWriteTransformNode from "./wasm-write-transform-node.js";
+export { wasmWriteTransformNode };
 
-import TxtWriteTransformNodeOptions from './txt-write-transform-node-options.js'
-export type { TxtWriteTransformNodeOptions }
+import WasmZstdReadTransformNodeResult from "./wasm-zstd-read-transform-node-result.js";
+export type { WasmZstdReadTransformNodeResult };
 
-import txtWriteTransformNode from './txt-write-transform-node.js'
-export { txtWriteTransformNode }
+import WasmZstdReadTransformNodeOptions from "./wasm-zstd-read-transform-node-options.js";
+export type { WasmZstdReadTransformNodeOptions };
 
+import wasmZstdReadTransformNode from "./wasm-zstd-read-transform-node.js";
+export { wasmZstdReadTransformNode };
 
-import WasmReadTransformNodeResult from './wasm-read-transform-node-result.js'
-export type { WasmReadTransformNodeResult }
+import WasmZstdWriteTransformNodeResult from "./wasm-zstd-write-transform-node-result.js";
+export type { WasmZstdWriteTransformNodeResult };
 
-import WasmReadTransformNodeOptions from './wasm-read-transform-node-options.js'
-export type { WasmReadTransformNodeOptions }
+import WasmZstdWriteTransformNodeOptions from "./wasm-zstd-write-transform-node-options.js";
+export type { WasmZstdWriteTransformNodeOptions };
 
-import wasmReadTransformNode from './wasm-read-transform-node.js'
-export { wasmReadTransformNode }
-
-
-import WasmWriteTransformNodeResult from './wasm-write-transform-node-result.js'
-export type { WasmWriteTransformNodeResult }
-
-import WasmWriteTransformNodeOptions from './wasm-write-transform-node-options.js'
-export type { WasmWriteTransformNodeOptions }
-
-import wasmWriteTransformNode from './wasm-write-transform-node.js'
-export { wasmWriteTransformNode }
-
-
-import WasmZstdReadTransformNodeResult from './wasm-zstd-read-transform-node-result.js'
-export type { WasmZstdReadTransformNodeResult }
-
-import WasmZstdReadTransformNodeOptions from './wasm-zstd-read-transform-node-options.js'
-export type { WasmZstdReadTransformNodeOptions }
-
-import wasmZstdReadTransformNode from './wasm-zstd-read-transform-node.js'
-export { wasmZstdReadTransformNode }
-
-
-import WasmZstdWriteTransformNodeResult from './wasm-zstd-write-transform-node-result.js'
-export type { WasmZstdWriteTransformNodeResult }
-
-import WasmZstdWriteTransformNodeOptions from './wasm-zstd-write-transform-node-options.js'
-export type { WasmZstdWriteTransformNodeOptions }
-
-import wasmZstdWriteTransformNode from './wasm-zstd-write-transform-node.js'
-export { wasmZstdWriteTransformNode }
+import wasmZstdWriteTransformNode from "./wasm-zstd-write-transform-node.js";
+export { wasmZstdWriteTransformNode };
