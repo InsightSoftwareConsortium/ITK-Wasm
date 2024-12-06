53dd7fa29c5fbdd600196b30102952088fa7af86
Matt McCormick <matt@mmmccormick.com>
2024-12-06 11:02:22 -0500

    feat(mesh-io): python mz3 emscripten support

--- packages/mesh-io/python/itkwasm-mesh-io-emscripten/itkwasm_mesh_io_emscripten/extension_to_point_set_io.py
+++ packages/mesh-io/python/itkwasm-mesh-io-emscripten/itkwasm_mesh_io_emscripten/extension_to_point_set_io.py
@@ -2,6 +2,7 @@ from collections import OrderedDict
 
 extension_to_point_set_io = OrderedDict([
     ('.vtk', 'vtkPolyData'),
+    ('.mz3', 'mz3'),
     ('.obj', 'obj'),
     ('.off', 'off'),
     ('.iwm', 'wasm'),
