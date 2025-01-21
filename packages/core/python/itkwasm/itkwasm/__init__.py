a15043adfbf45cf9145104f60dd584dae79b74a5
Matt McCormick <matt@mmmccormick.com>
2025-01-21 11:31:14 -0500

    chore(itkwasm): bump version to 1.0b186

--- packages/core/python/itkwasm/itkwasm/__init__.py
+++ packages/core/python/itkwasm/itkwasm/__init__.py
@@ -1,6 +1,6 @@
 """itkwasm: Python interface to itk-wasm WebAssembly modules."""
 
-__version__ = "1.0b185"
+__version__ = "1.0b186"
 
 from .interface_types import InterfaceTypes
 from .image import Image, ImageType, ImageRegion
