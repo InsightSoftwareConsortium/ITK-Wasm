999c42e343976c877fa9bcb2407642d6ae0f3283
Hans Johnson <hans-johnson@uiowa.edu>
2025-01-26 09:03:34 -0600

    style(itk-wasm): Add itkVirtualGetNameOfClassMacro + itkOverrideGetNameOfClassMacro
    Added two new macro's, intended to replace the old 'itkTypeMacro' and
    'itkTypeMacroNoParent'.
    
    The main aim is to be clearer about what those macro's do: add a virtual
    'GetNameOfClass()' member function and override it. Unlike 'itkTypeMacro',
    'itkOverrideGetNameOfClassMacro' does not have a 'superclass' parameter, as it
    was not used anyway.
    
    Note that originally 'itkTypeMacro' did not use its 'superclass' parameter
    either, looking at commit 699b66cb04d410e555656828e8892107add38ccb, Will
    Schroeder, June 27, 2001:
    https://github.com/InsightSoftwareConsortium/ITK/blob/699b66cb04d410e555656828e8892107add38ccb/Code/Common/itkMacro.h#L331-L337

--- include/itkWasmTransform.h
+++ include/itkWasmTransform.h
@@ -57,7 +57,7 @@ public:
 
   itkNewMacro(Self);
   /** Run-time type information (and related methods). */
-  itkTypeMacro(WasmTransform, WasmDataObject);
+  itkOverrideGetNameOfClassMacro(WasmTransform);
 
   using TransformType = TTransform;
   using DecoratorType = DataObjectDecorator<TransformType>;
