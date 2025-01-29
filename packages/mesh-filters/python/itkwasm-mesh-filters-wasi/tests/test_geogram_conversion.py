60f054a69ec450b2bfe106e81717a2a1aed5f77e
Matt McCormick <matt@mmmccormick.com>
2025-01-29 10:57:01 -0500

    test(mesh-filters): define test_repair.py

---
+++ packages/mesh-filters/python/itkwasm-mesh-filters-wasi/tests/test_geogram_conversion.py
@@ -0,0 +1,8 @@
+# Generated file. To retain edits, remove this comment.
+
+from itkwasm_mesh_filters_wasi import geogram_conversion
+
+from .common import test_input_path, test_output_path
+
+def test_geogram_conversion():
+    pass
---
+++ packages/mesh-filters/python/itkwasm-mesh-filters-wasi/tests/test_keep_largest_component.py
@@ -0,0 +1,8 @@
+# Generated file. To retain edits, remove this comment.
+
+from itkwasm_mesh_filters_wasi import keep_largest_component
+
+from .common import test_input_path, test_output_path
+
+def test_keep_largest_component():
+    pass
