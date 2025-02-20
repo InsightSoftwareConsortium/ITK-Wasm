5cde5777e48d44cedc865eb5f74f45c88ad676bd
Matt McCormick <matt@mmmccormick.com>
2025-02-20 15:26:54 -0500

    docs(hello-world-example): use itk::wasm::Pipeline
    A best practice for usefulness for any next step.

--- examples/hello-world/hello.cxx
+++ examples/hello-world/hello.cxx
@@ -1,6 +1,10 @@
 #include <iostream>
 
-int main() {
+#include "itkPipeline.h"
+
+int main(int argc, char* argv[]) {
+  itk::wasm::Pipeline pipeline("hello-world", "Hello world example", argc, argv);
+
   std::cout << "Hello Wasm world!" << std::endl;
   return 0;
 }
