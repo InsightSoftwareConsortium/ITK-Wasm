322b18d40d46eff9223e1bc0109ffb0f1209ae71
Matthew McCormick <matt@mmmccormick.com>
2025-06-12 17:05:10 -0400

    chore(create-itk-wasm): bump pnpm, itkwasm

--- packages/core/typescript/create-itk-wasm/src/generate/pixi-toml.ts
+++ packages/core/typescript/create-itk-wasm/src/generate/pixi-toml.ts
@@ -23,7 +23,7 @@ python = ["python"]
 
 [dependencies]
 python = "3.12.*"
-pnpm = ">=9.12.1,<10"
+pnpm = ">=10.12.1,<11"
 hatch = ">=1.13.0,<2"
 pip = ">=24.2,<25"
 
@@ -94,7 +94,7 @@ pytest = ">=8.3.3,<9"
 [feature.python.pypi-dependencies]
 pyodide-py = ">=0.26.3, <0.27"
 pytest-pyodide = ">=0.58.3, <0.59"
-itkwasm = ">=1.0b185, <2"
+itkwasm = ">=1.0b189, <2"
 
 [tasks.build-python]
 cmd = "pnpm run build:gen:python"
