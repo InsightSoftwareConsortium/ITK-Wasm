3abcfff0d2eb6c70f5944fccdb99ce7416aa7fd8
Matt McCormick <matt@fideus.io>
2025-08-15 16:07:15 -0400

    feat(load-emscripten-module-node): support pthread modules

--- packages/core/typescript/itk-wasm/src/pipeline/internal/load-emscripten-module-node.ts
+++ packages/core/typescript/itk-wasm/src/pipeline/internal/load-emscripten-module-node.ts
@@ -2,6 +2,7 @@ import fs from 'fs'
 import EmscriptenModule from '../itk-wasm-emscripten-module.js'
 import { pathToFileURL } from 'url'
 import { ZSTDDecoder } from '@thewtex/zstddec'
+import pthreadSupportAvailable from '../pthread-support-available.js'
 
 const zstdDecoder = new ZSTDDecoder()
 await zstdDecoder.init()
@@ -19,9 +20,49 @@ async function loadEmscriptenModuleNode (
   if (modulePath.endsWith('.wasm.zst')) {
     modulePrefix = modulePath.substring(0, modulePath.length - 9)
   }
-  const compressedWasmBinaryPath = `${modulePrefix}.wasm.zst`
-  const compressedWasmBinary = fs.readFileSync(compressedWasmBinaryPath)
-  const wasmBinary = zstdDecoder.decode(new Uint8Array(compressedWasmBinary))
+
+  // Check for pthread support and use the appropriate WASM file
+  const hasPthreadSupport = pthreadSupportAvailable()
+  let wasmFileName = `${modulePrefix}.wasm.zst`
+  let wasmBinary: Uint8Array
+
+  if (hasPthreadSupport) {
+    const threadsWasmPath = `${modulePrefix}.threads.wasm.zst`
+    if (fs.existsSync(threadsWasmPath)) {
+      wasmFileName = threadsWasmPath
+      const compressedWasmBinary = fs.readFileSync(wasmFileName)
+      wasmBinary = zstdDecoder.decode(new Uint8Array(compressedWasmBinary))
+    } else {
+      // Fall back to checking for compressed non-threaded version
+      if (fs.existsSync(wasmFileName)) {
+        const compressedWasmBinary = fs.readFileSync(wasmFileName)
+        wasmBinary = zstdDecoder.decode(new Uint8Array(compressedWasmBinary))
+      } else {
+        // Fall back to uncompressed WASM file
+        const uncompressedWasmPath = `${modulePrefix}.wasm`
+        if (fs.existsSync(uncompressedWasmPath)) {
+          wasmBinary = fs.readFileSync(uncompressedWasmPath)
+        } else {
+          throw new Error(`No WASM file found for module: ${modulePrefix}`)
+        }
+      }
+    }
+  } else {
+    // Check for compressed non-threaded version first
+    if (fs.existsSync(wasmFileName)) {
+      const compressedWasmBinary = fs.readFileSync(wasmFileName)
+      wasmBinary = zstdDecoder.decode(new Uint8Array(compressedWasmBinary))
+    } else {
+      // Fall back to uncompressed WASM file
+      const uncompressedWasmPath = `${modulePrefix}.wasm`
+      if (fs.existsSync(uncompressedWasmPath)) {
+        wasmBinary = fs.readFileSync(uncompressedWasmPath)
+      } else {
+        throw new Error(`No WASM file found for module: ${modulePrefix}`)
+      }
+    }
+  }
+
   const fullModulePath = pathToFileURL(`${modulePrefix}.js`).href
   const result = await import(
     /* webpackIgnore: true */ /* @vite-ignore */ fullModulePath
