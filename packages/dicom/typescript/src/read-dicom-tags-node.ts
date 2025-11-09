dfb2c34fc3a163c465a8f18f8b2b0a74aee02b74
Matt McCormick <matt@fideus.io>
2025-11-09 11:32:14 -0500

    fix(dicom): use fileURLToPath in read-dicom-tags-node

--- packages/dicom/typescript/src/read-dicom-tags-node.ts
+++ packages/dicom/typescript/src/read-dicom-tags-node.ts
@@ -3,14 +3,14 @@ import {
   InterfaceTypes,
   PipelineOutput,
   PipelineInput,
-  runPipelineNode
-} from 'itk-wasm'
+  runPipelineNode,
+} from "itk-wasm";
 
-import ReadDicomTagsOptions from './read-dicom-tags-options.js'
-import ReadDicomTagsNodeResult from './read-dicom-tags-node-result.js'
+import ReadDicomTagsOptions from "./read-dicom-tags-options.js";
+import ReadDicomTagsNodeResult from "./read-dicom-tags-node-result.js";
 
-
-import path from 'path'
+import path from "path";
+import { fileURLToPath } from "url";
 
 /**
  * Read the tags from a DICOM file
@@ -23,51 +23,58 @@ import path from 'path'
 async function readDicomTagsNode(
   dicomFile: string,
   options: ReadDicomTagsOptions = {}
-) : Promise<ReadDicomTagsNodeResult> {
-
-  const mountDirs: Set<string> = new Set()
+): Promise<ReadDicomTagsNodeResult> {
+  const mountDirs: Set<string> = new Set();
 
   const desiredOutputs: Array<PipelineOutput> = [
     { type: InterfaceTypes.JsonCompatible },
-  ]
+  ];
 
-  mountDirs.add(path.dirname(dicomFile as string))
-  const inputs: Array<PipelineInput> = [
-  ]
+  mountDirs.add(path.dirname(dicomFile as string));
+  const inputs: Array<PipelineInput> = [];
 
-  const args = []
+  const args = [];
   // Inputs
-  const dicomFileName = dicomFile
-  args.push(dicomFileName as string)
+  const dicomFileName = dicomFile;
+  args.push(dicomFileName as string);
 
   // Outputs
-  const tagsName = '0'
-  args.push(tagsName)
+  const tagsName = "0";
+  args.push(tagsName);
 
   // Options
-  args.push('--memory-io')
+  args.push("--memory-io");
   if (typeof options.tagsToRead !== "undefined") {
-    const inputCountString = inputs.length.toString()
-    inputs.push({ type: InterfaceTypes.JsonCompatible, data: options.tagsToRead as JsonCompatible })
-    args.push('--tags-to-read', inputCountString)
-
+    const inputCountString = inputs.length.toString();
+    inputs.push({
+      type: InterfaceTypes.JsonCompatible,
+      data: options.tagsToRead as JsonCompatible,
+    });
+    args.push("--tags-to-read", inputCountString);
   }
 
-  const pipelinePath = path.join(path.dirname(import.meta.url.substring(7)), 'pipelines', 'read-dicom-tags')
+  const pipelinePath = path.join(
+    fileURLToPath(import.meta.url),
+    "..",
+    "pipelines",
+    "read-dicom-tags"
+  );
 
-  const {
-    returnValue,
-    stderr,
-    outputs
-  } = await runPipelineNode(pipelinePath, args, desiredOutputs, inputs, mountDirs)
+  const { returnValue, stderr, outputs } = await runPipelineNode(
+    pipelinePath,
+    args,
+    desiredOutputs,
+    inputs,
+    mountDirs
+  );
   if (returnValue !== 0) {
-    throw new Error(stderr)
+    throw new Error(stderr);
   }
 
   const result = {
     tags: outputs[0].data as [string, string][],
-  }
-  return result
+  };
+  return result;
 }
 
-export default readDicomTagsNode
+export default readDicomTagsNode;
